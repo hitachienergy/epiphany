7a9b2eb58bb7093522cd7575d03b410eeca373c9
Przemek <przemyslaw.dyrda@pl.abb.com>
2019-02-18 14:39:04 +0100

    Fixes after review

--- core/core/test/serverspec/spec/master/master_spec.rb
+++ core/core/test/serverspec/spec/master/master_spec.rb
@@ -1,76 +1,80 @@
 require 'spec_helper'
-require "base64"
+require 'base64'
 
 
-describe 'Check if kubelet service is up' do
+describe 'Checking if kubelet service is running' do
   describe service('kubelet') do
     it { should be_enabled }
     it { should be_running }
   end
 end
 
-describe 'Check if kube-scheduler is up' do
-  describe process("kube-scheduler") do
+describe 'Checking if kube-scheduler is running' do
+  describe process('kube-scheduler') do
     it { should be_enabled }
     it { should be_running }
   end
 end
 
-describe 'Check if kube-scheduler is up - 2nd test' do
+describe 'Checking if kube-scheduler is on process list' do
   describe command('ps -C kube-scheduler --no-headers') do
     its(:stdout) { should match /kube-scheduler/ }
     its(:exit_status) { should eq 0 }
   end
 end
 
-describe 'Check if kube-controller is up' do
-  describe process("kube-controller") do
+describe 'Checking if kube-controller is running' do
+  describe process('kube-controller') do
     it { should be_enabled }
     it { should be_running }
   end
 end
 
-describe 'Check if kube-controller is up - 2nd test' do
+describe 'Checking if kube-controller is on process list' do
   describe command('ps -C kube-controller --no-headers') do
     its(:stdout) { should match /kube-controller/ }
     its(:exit_status) { should eq 0 }
   end
 end
 
-describe 'Check if kube-apiserver is up' do
+describe 'Checking if kube-apiserver is running' do
   describe process("kube-apiserver") do
     it { should be_enabled }
     it { should be_running }
   end
 end
 
-describe 'Check if kube-apiserver is up - 2nd test' do
+describe 'Checking if kube-apiserver is on process list' do
   describe command('ps -C kube-apiserver --no-headers') do
     its(:stdout) { should match /kube-apiserver/ }
     its(:exit_status) { should eq 0 }
   end
 end
 
-describe 'Check if there are any pods that have status other than Running' do
+describe 'Checking if there are any pods that have status other than Running' do
   describe command('kubectl get pods --all-namespaces --field-selector=status.phase!=Running') do
-    its(:stdout) { should match // }
+    its(:stdout) { should match /^$/ }
     its(:stderr) { should match /No resources found./ }
   end
 end  
 
-describe 'Check if the number of master nodes is the same as indicated in the inventory file' do
+describe 'Checking if the number of master nodes is the same as indicated in the inventory file' do
   describe command('kubectl get nodes --selector=node-role.kubernetes.io/master --no-headers | wc -l | tr -d "\n"') do
-    its(:stdout) { should eq count_inventory_roles("master").to_s }
+    it "is expected to be equal" do
+      expect(subject.stdout.to_i).to eq count_inventory_roles("master")
+      end
   end
 end
 
-describe 'Check if the number of worker nodes is the same as indicated in the inventory file' do
+describe 'Checking if the number of worker nodes is the same as indicated in the inventory file' do
   describe command('kubectl get nodes --no-headers | grep -v master | wc -l | tr -d "\n"') do
-    its(:stdout) { should eq count_inventory_roles("worker").to_s }
+    it "is expected to be equal" do
+      expect(subject.stdout.to_i).to eq count_inventory_roles("worker")
+      end
   end
 end
 
-describe 'Check if there are any nodes that have status other than Ready' do
+describe 'Checking if there are any nodes that have status other than Ready' do
   describe command('kubectl get nodes') do
     its(:stdout) { should match /Ready/ }
     its(:stdout) { should_not match /NotReady/ }
@@ -79,14 +83,14 @@ describe 'Check if there are any nodes that have status other than Ready' do
   end
 end  
 
-describe 'Check the port on which to serve HTTPS with authentication and authorization' do
+describe 'Checking the port on which to serve HTTPS with authentication and authorization' do
   describe port(6443) do
     let(:disable_sudo) { false }
     it { should be_listening }
   end
 end  
 
-describe 'Creating, checking and decoding a secret using kubectl' do
+describe 'Checking secret creation using kubectl' do
 
   test_user = 'user123'
   test_pass = 'pass456'
@@ -95,17 +99,17 @@ describe 'Creating, checking and decoding a secret using kubectl' do
   timestamp = DateTime.now.to_time.to_i.to_s
   test_secret = 'testsecret' + timestamp
 
-  describe 'Creating a secret...' do
+  describe 'Checking if the secret was successfully created' do
     describe command("kubectl create secret generic #{test_secret} --from-literal=username=#{test_user} --from-literal=password=#{test_pass}") do
       its(:stdout) { should match /secret\/#{test_secret} created/ }
     end
   end
-  describe 'Checking if it exists...' do
-    describe command("kubectl get secrets") do
+  describe 'Checking if the created secret is present on the list with all secrets' do
+    describe command('kubectl get secrets') do
       its(:stdout) { should match /#{test_secret}/ }
     end
   end
-  describe 'Decoding...' do
+  describe 'Checking if the secret stores encoded data' do
     describe command("kubectl get secret #{test_secret} -o yaml") do
       its(:stdout) { should match /name: #{test_secret}/ }
       its(:stdout) { should match /#{test_user_b64}/ }
@@ -114,40 +118,46 @@ describe 'Creating, checking and decoding a secret using kubectl' do
   end
 end  
 
-describe 'Testing kubernetes dashboard...' do
-  describe 'Looking for kubernetes-dashboard pod' do
+describe 'Checking kubernetes dashboard availability' do
+  describe 'Checking if kubernetes-dashboard pod is running' do
     describe command('kubectl get pods --all-namespaces | grep kubernetes-dashboard') do
         its(:stdout) { should match /kubernetes-dashboard/ }
         its(:stdout) { should match /Running/ }
         its(:exit_status) { should eq 0 }
     end
   end
-  describe 'Checking kubernetes-dashboard deployment status' do
+  describe 'Checking if kubernetes-dashboard is deployed' do
     describe command('kubectl get deployments --all-namespaces --field-selector metadata.name=kubernetes-dashboard') do
       its(:stdout) { should match /kubernetes-dashboard/ }
       its(:stdout) { should match /1\/1/ }
       its(:exit_status) { should eq 0 }
     end
   end
-  describe 'Getting admin token bearer' do
-    describe command("kubectl describe secret $(kubectl get secrets --namespace=kube-system | grep admin-user \
-    | awk '{print $1}') --namespace=kube-system | grep -E '^token' | awk '{print $2}' | head -1") do
+  describe 'Checking if admin token bearer exists' do
+    describe command("kubectl describe secret $(kubectl get secrets --namespace=kube-system | grep addmin-user \
+    | awk '{print $1}') --namespace=kube-system | awk '/^token/ {print $2}' | head -1") do
       its(:stdout) { should_not match /^$/ }
-    end
-  end
-  describe 'Setting up proxy and starting to serve on localhost' do
-    describe command("kubectl proxy >/dev/null 2>&1 &") do
       its(:exit_status) { should eq 0 }
     end
   end
-  describe 'Checking the dashboard availability' do
+  describe 'Setting up proxy and starting to serve on localhost' do
+    describe command('kubectl proxy >/dev/null 2>&1 &') do
+      its(:exit_status) { should eq 0 }
+    end
+  end
+  describe 'Checking if the dashboard is available' do
     describe command("curl -I 'http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/'") do
       its(:stdout) { should match /HTTP\/1.1 200 OK/ }
     end
   end
+  describe 'Terminating kubectl proxy process' do
+    describe command('pkill -f "kubectl proxy"') do
+      its(:exit_status) { should eq 0 }
+    end
+  end
 end
 
-describe 'Checking coredns deployment status' do
+describe 'Checking if coredns is deployed' do
   describe command('kubectl get deployments --all-namespaces --field-selector metadata.name=coredns') do
     coredns_counter = 2 # always equal 2
     its(:stdout) { should match /coredns/ }
@@ -156,3 +166,9 @@ describe 'Checking coredns deployment status' do
   end
 end
 
+describe 'Checking if kubernetes healthz endpoint is responding' do
+  describe command('curl --insecure -I -m 2 https://127.0.0.1:10250/healthz') do
+    its(:stdout) { should match /HTTP\/2 401/ }
+    its(:exit_status) { should eq 0 }
+  end
+end
