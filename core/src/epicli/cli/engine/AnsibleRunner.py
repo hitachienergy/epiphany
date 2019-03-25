5ed9520dc75c985bdeb4e15773f92b996e5c0e0a
toszo <tomasz.szostak@gmail.com>
2019-03-25 16:05:00 +0100

    Creator for ansible inventory

---
+++ core/src/epicli/cli/engine/AnsibleRunner.py
@@ -0,0 +1,22 @@
+from cli.engine.AnsibleInventoryCreator import AnsibleInventoryCreator
+
+
+class AnsibleRunner:
+    def __init__(self, cluster_model, config_docs):
+        self.cluster_model = cluster_model
+        self.config_docs = config_docs
+        self.inventory_creator = AnsibleInventoryCreator(cluster_model, config_docs)
+
+    def __enter__(self):
+        self.inventory_creator.__enter__()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.inventory_creator.__exit__(exc_type, exc_value, traceback)
+
+    def run(self):
+        inventory = self.inventory_creator.create()
+        # todo run ansible playbooks
+
+
+
--- core/src/epicli/cli/helpers/build_saver.py
+++ core/src/epicli/cli/helpers/build_saver.py
@@ -3,12 +3,26 @@ import os
 
 BUILD_FOLDER_PATH = '../../build/'
 OUTPUT_FILE_NAME = 'manifest.yml'
+INVENTORY_FILE_NAME = 'inventory'
 
 
-def save_build(docs, context):
+def save_build(docs, cluster_name):
     script_dir = os.path.dirname(__file__)
-    build_directory = os.path.join(script_dir, BUILD_FOLDER_PATH, context)
+    build_directory = os.path.join(script_dir, BUILD_FOLDER_PATH, cluster_name)
     if not os.path.exists(build_directory):
         os.makedirs(build_directory)
     with open(os.path.join(build_directory, OUTPUT_FILE_NAME), 'w') as stream:
         dump_all(docs, stream)
+
+
+def save_inventory(inventory, cluster_name):
+    script_dir = os.path.dirname(__file__)
+    build_directory = os.path.join(script_dir, BUILD_FOLDER_PATH, cluster_name)
+    if not os.path.exists(build_directory):
+        os.makedirs(build_directory)
+    with open(os.path.join(build_directory, INVENTORY_FILE_NAME), 'w') as file:
+        for item in inventory:
+            file.write("["+item.role+"]\n")
+            for host in item.hosts:
+                file.write(host.name+" ansible_host="+host.ip+"\n")
+
