f1b16a0f5950436047dccfcc09be287742a342b7
erzetpe <erzetpe@gmail.com>
2019-04-11 11:14:49 +0200

    Rename playbooks to new roles, add component mapping for files in ansible (#205)
    * Item: #58 Desc: Change template to match query parameter from ansible inventory creation
    
    * Item: #58 Desc: Changed list creation to more pythonic way
    
    * Item: #58 Desc: Fix issue with terraform file helper
    
    * Item: #58 Desc: Code refactor and moving code to TerraformRunner class
    
    * Item: #58 Desc: Add waiting for ansible inventory creation
    
    * Item: #58 Desc: Change name of inventory existing variable
    
    * Item: #58 Desc: Fixes with paths to libraries
    
    * Item: #59 Desc: Add initial template to generate new inventory
    
    * Item: #59 Desc: Change ansible inventory to template
    
    * Item: #59 Desc: Refactoring of code
    
    * Item: #59 Desc: Add runner execution
    
    * Item: #59 Desc: Add working ansible runner code
    
    * Item: #59 Desc: Add copy of ansible playbooks
    
    * Item: #59 Desc: Add ansible playbooks execution
    
    * Item: #59 Desc: Add integration with ansible roles

--- core/src/epicli/cli/engine/AnsibleRunner.py
+++ core/src/epicli/cli/engine/AnsibleRunner.py
@@ -6,9 +6,11 @@ import ansible_runner
 from cli.engine.AnsibleInventoryCreator import AnsibleInventoryCreator
 from cli.helpers.Step import Step
 from cli.helpers.build_saver import get_inventory_path, get_ansible_path, copy_files_recursively
+from cli.helpers.role_name_helper import adjust_name
 
 
 class AnsibleRunner(Step):
+
     ANSIBLE_PLAYBOOKS_PATH = "/../../../../core/src/ansible/"
 
     def __init__(self, cluster_model, config_docs):
@@ -18,7 +20,7 @@ class AnsibleRunner(Step):
         self.inventory_creator = AnsibleInventoryCreator(cluster_model, config_docs, use_public_ips=True)
 
     def __enter__(self):
-        super().__enter__();
+        super().__enter__()
         self.inventory_creator.__enter__()
         return self
 
@@ -33,7 +35,7 @@ class AnsibleRunner(Step):
             if_inventory_exists_and_have_content = os.path.exists(inventory_path) and os.path.getsize(
                 inventory_path) > 0
             if if_inventory_exists_and_have_content:
-                continue
+                break
 
             self.inventory_creator.create()
             time.sleep(10)
@@ -43,7 +45,7 @@ class AnsibleRunner(Step):
         copy_files_recursively(src, get_ansible_path(self.cluster_model.specification.name))
 
         # todo: install packages to run ansible on Red Hat hosts
-        for i in range(10):
+        for i in range(2):
             runner = ansible_runner.run(private_data_dir=get_ansible_path(self.cluster_model.specification.name),
                                         host_pattern="all", inventory=inventory_path,
                                         module='raw', module_args='sudo apt-get install -y python-simplejson')
@@ -54,7 +56,7 @@ class AnsibleRunner(Step):
 
             time.sleep(10)
 
-        for i in range(10):
+        for i in range(2):
             runner = ansible_runner.run(private_data_dir=get_ansible_path(self.cluster_model.specification.name),
                                         host_pattern="all", inventory=inventory_path,
                                         playbook=os.path.join(get_ansible_path(self.cluster_model.specification.name),
@@ -70,10 +72,13 @@ class AnsibleRunner(Step):
         for component in self.cluster_model.specification["components"]:
             print(component)
 
-            runner = ansible_runner.run(private_data_dir=get_ansible_path(self.cluster_model.specification.name),
-                                        host_pattern="all", inventory=inventory_path,
-                                        playbook=os.path.join(get_ansible_path(self.cluster_model.specification.name),
-                                                              component + ".yml"))
+            roles = self.inventory_creator.get_roles_for_feature(component_key=component)
+
+            for role in roles:
+                runner = ansible_runner.run(private_data_dir=get_ansible_path(self.cluster_model.specification.name),
+                                            host_pattern="all", inventory=inventory_path,
+                                            playbook=os.path.join(get_ansible_path(self.cluster_model.specification.name),
+                                                              adjust_name(role) + ".yml"))
 
             print(runner.status)
 
