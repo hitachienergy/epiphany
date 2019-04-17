6dc916c2aa90457c639c0f6b22110ec45f782dee
Luuk van Venrooij <luukvanvenrooij84@gmail.com>
2019-04-17 22:45:41 +0200

    Added some additional parameters for logging.

--- core/src/epicli/cli/epicli.py
+++ core/src/epicli/cli/epicli.py
@@ -13,9 +13,15 @@ def main(arguments):
         usage='''epicli <command> [<args>]''',
         formatter_class=argparse.RawDescriptionHelpFormatter)
 
-    # setup root arguments
-    parser.add_argument('-l', '--log', dest='log_name', type=str,
+    # setup some root arguments
+    parser.add_argument('-l', '--log_file', dest='log_name', type=str,
                         help='The name of the log file written to the output directory')
+    parser.add_argument('-lf', '--log_format', dest='log_format', type=str,
+                        help='Format for the logging string.')
+    parser.add_argument('-ldf', '--log_date_format', dest='log_date_format', type=str,
+                        help='Format for the logging date.')
+    parser.add_argument('-lc', '--log_count', dest='log_count', type=str,
+                        help='Roleover count where each CLI run will generate a new log.')
     # some arguments we don't want available when running from the docker image.
     if not config.docker_cli:
         parser.add_argument('-o', '--output', dest='output_dir', type=str,
@@ -29,6 +35,9 @@ def main(arguments):
     args = parser.parse_args(arguments)
     config.output_dir = args.output_dir if hasattr(args, 'output_dir') else None
     config.log_file = args.log_name
+    config.log_format = args.log_format
+    config.log_date_format = args.log_date_format
+    config.log_count = args.log_count
     dump_config(config)
 
     return args.func(args)
@@ -49,7 +58,7 @@ def dump_config(config):
     logger = Log('config')
     for attr in config.__dict__:
         if attr.startswith('_'):
-             logger.info('%s = %r' % (attr[1:], getattr(config, attr)))
+             logger.debug('%s = %r' % (attr[1:], getattr(config, attr)))
 
 
 if __name__ == '__main__':
--- core/src/epicli/cli/helpers/Config.py
+++ core/src/epicli/cli/helpers/Config.py
@@ -4,10 +4,19 @@ class Config:
     class __ConfigBase:
         def __init__(self):
             self._docker_cli = os.environ.get('DOCKER_CLI', False)
-            self._log_file = 'log.json'
+
             self._output_dir = os.path.join(os.path.dirname(__file__), '../../output/')
             if self._docker_cli:
                 self._output_dir = '/shared/'
+
+            self._log_file = 'log.json'
+            self._log_format = '%(asctime)s %(levelname)s %(name)s - %(message)s'
+            self._log_date_format = '%H:%M:%S'
+            self._log_count = 10
+
+        @property
+        def docker_cli(self):
+            return self._docker_cli
 
         @property
         def output_dir(self):
@@ -15,9 +24,8 @@ class Config:
 
         @output_dir.setter
         def output_dir(self, output_dir):
-            if self._docker_cli or output_dir is None:
-                return
-            self._output_dir = output_dir
+            if not self._docker_cli and not output_dir is None:
+                self._output_dir = output_dir
 
         @property
         def log_file(self):
@@ -25,13 +33,37 @@ class Config:
 
         @log_file.setter
         def log_file(self, log_file):
-            if log_file is None:
-                return
-            self._log_file = log_file
+            if not log_file is None:
+                self._log_file = log_file
 
         @property
-        def docker_cli(self):
-            return self._docker_cli
+        def log_format(self):
+            return self._log_format
+
+        @log_format.setter
+        def log_format(self, log_format):
+            if not log_format is None:
+                self._log_format = log_format
+
+        @property
+        def log_date_format(self):
+            return self._log_date_format
+
+        @log_date_format.setter
+        def log_date_format(self, log_date_format):
+            if not log_date_format is None:
+                self._log_date_format = log_date_format
+
+        @property
+        def log_count(self):
+            return self._log_count
+
+        @log_count.setter
+        def log_count(self, log_count):
+            if not log_count is None:
+                self._log_count = log_count
+
+
 
     instance = None
 
