d95faeca9bc7eb62361a2dfbcd89c40096c73d78
Luuk van Venrooij <luukvanvenrooij84@gmail.com>
2019-07-10 14:09:45 +0200

    Fixes for removed import.

--- core/src/epicli/cli/helpers/build_saver.py
+++ core/src/epicli/cli/helpers/build_saver.py
@@ -1,5 +1,6 @@
 import distutils
 import shutil
+from distutils import dir_util
 import os
 from cli.helpers.data_loader import load_template_file, types
 from cli.helpers.yaml_helpers import dump_all
