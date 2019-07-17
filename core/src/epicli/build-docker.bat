7dcff82a139fec9ecc6da48d162eafb0b9fb1f72
Luuk van Venrooij <11056665+seriva@users.noreply.github.com>
2019-07-17 14:23:43 +0200

    BDS and test setup update. (#358)
    * Import fix for setup.py
    
    * Fix for POSIX guidelines/standards for CLI parameters
    
    * Fixes for return codes and error handling. Added confirmation for experimental features.
    
    * Fixes for return codes and error handling. Added confirmation for experimental features.
    
    * Fixes for removed import.
    
    * Fix for removed import
    
    * Fix for accidental debug statement.
    
    * Throw error for unknown ssh key path.
    
    * - Fixed data path issue for Ansible playbooks.
    - Fixed missing ssh dependency for epicli container.
    
    * Fix for tar and unzip
    
    * Added flag to control https certificate validation for certain Ansible operations.
    
    * - Added scripts to prepare for BDS.
    - Fixed Configuration issue for tests.
    - Added documentation for scripts.

--- core/src/epicli/build-docker.bat
+++ core/src/epicli/build-docker.bat
@@ -1,2 +1,4 @@
+:: Script to build the Epicli docker image. It wil build the Epicli distribtion wheel 
+:: and from use that to provision the docker image.
 call build-wheel.bat
 docker build -t epicli -f Dockerfile .
