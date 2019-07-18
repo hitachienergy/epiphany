edf9d86d58be72476e375f55aa9f36a2bf776cef
Luuk van Venrooij <luukvanvenrooij84@gmail.com>
2019-07-18 09:16:51 +0200

    Minor fix for BDS preparation script.

--- core/src/epicli/prepare-bds.bat
+++ core/src/epicli/prepare-bds.bat
@@ -2,4 +2,6 @@
 :: and from that download and build/extract all used python dependencies
 :: to the externals directory for inclusion with the BDS scan.
 call build-wheel.bat
-pip download --no-clean --no-binary all -d %cd%/external/packages/ --build %cd%/external/build/ %cd%/dist/epicli-0.3.0-py3-none-any.whl
+pip download --no-clean --no-binary all -d %cd%/external/packages/ --build %cd%/external/ %cd%/dist/epicli-0.3.0-py3-none-any.whl
+rmdir /Q /S %cd%\external\packages\
+rmdir /Q /S %cd%\external\epicli\
