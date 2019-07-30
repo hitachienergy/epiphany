a7b41760210a493b7df4918d55e3f91a6c22ea15
Przemek <przemyslaw.dyrda@pl.abb.com>
2019-07-30 17:29:58 +0200

    added quotes to variables

--- core/core/src/docker/test-CI-AWS/run.sh
+++ core/core/src/docker/test-CI-AWS/run.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 function destroyEnvironment {
-	cd /shared/$CLUSTER_NAME/terraform
+	cd "/shared/$CLUSTER_NAME/terraform"
 	terraform init
 	terraform destroy -auto-approve
 }
 
 echo "Starting Epiphany build for cluster $CLUSTER_NAME"
 
-epicli apply -f /shared/$CLUSTER_NAME/$CLUSTER_NAME.yml
+epicli apply -f "/shared/$CLUSTER_NAME/$CLUSTER_NAME.yml"
 
 status=$?
 
-if [ $status -eq 0 ]
+if [[ $status -eq 0 ]]
 then
 	echo
 	echo "Epiphany build for cluster $CLUSTER_NAME completed successfully"
 	echo
 	echo "Serverspec tests for cluster $CLUSTER_NAME started..."
-	rake inventory=/shared/$CLUSTER_NAME/inventory user=$ADMIN_USERNAME keypath=$KEY_PATH spec:all
+	rake inventory="/shared/$CLUSTER_NAME/inventory" user=$ADMIN_USERNAME keypath=$KEY_PATH spec:all
 	echo "Serverspec tests for cluster $CLUSTER_NAME finished"
 	destroyEnvironment 
 	echo
