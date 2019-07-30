3918d69b37707cfb03d5a060182b76a3d86456b9
Przemek <przemyslaw.dyrda@pl.abb.com>
2019-07-30 17:00:14 +0200

    modifying run script

--- core/core/src/docker/test-CI-AWS/run.sh
+++ core/core/src/docker/test-CI-AWS/run.sh
@@ -6,6 +6,8 @@ function destroyEnvironment {
 	terraform destroy -auto-approve
 }
 
+echo "Starting Epiphany build for cluster $CLUSTER_NAME"
+
 epicli apply -f /shared/$CLUSTER_NAME/$CLUSTER_NAME.yml
 
 status=$?
@@ -13,7 +15,7 @@ status=$?
 if [ $status -eq 0 ]
 then
 	echo
-	echo "Epiphany build for cluster $CLUSTER_NAME completed"
+	echo "Epiphany build for cluster $CLUSTER_NAME completed successfully"
 	echo
 	echo "Serverspec tests for cluster $CLUSTER_NAME started..."
 	rake inventory=/shared/$CLUSTER_NAME/inventory user=$ADMIN_USERNAME keypath=$KEY_PATH spec:all
