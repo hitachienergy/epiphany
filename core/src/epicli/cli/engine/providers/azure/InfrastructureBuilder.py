c61561da0fe1e66f272b40f90e4d2fa207ff17a1
Luuk van Venrooij <luukvanvenrooij84@gmail.com>
2019-08-12 21:09:35 +0200

    Moved RG creation to separate configuration.

--- core/src/epicli/cli/engine/providers/azure/InfrastructureBuilder.py
+++ core/src/epicli/cli/engine/providers/azure/InfrastructureBuilder.py
@@ -1,13 +1,35 @@
 from cli.helpers.Step import Step
-
+from cli.helpers.naming_helpers import resource_name
+from cli.helpers.doc_list_helpers import select_single, select_all
+from cli.helpers.doc_list_helpers import select_first
+from cli.helpers.data_loader import load_yaml_obj, types
 
 class InfrastructureBuilder(Step):
     def __init__(self, docs):
         super().__init__(__name__)
+        self.cluster_model = select_single(docs, lambda x: x.kind == 'epiphany-cluster')
+        self.cluster_name = self.cluster_model.specification.name.lower()
+        self.cluster_prefix = self.cluster_model.specification.prefix.lower()
         self.docs = docs
 
     def run(self):
         infrastructure = []
 
+        resource_group = self.get_resource_group()
+        infrastructure.append(resource_group)        
+
         return infrastructure
+
+    def get_resource_group(self):
+        resource_group = self.get_config_or_default(self.docs, 'infrastructure/resource-group')
+        resource_group.specification.name = resource_name(self.cluster_prefix, self.cluster_name, 'rg')
+        resource_group.specification.region = self.cluster_model.specification.cloud.region
+        return resource_group    
+    
+    @staticmethod
+    def get_config_or_default(docs, kind):
+        config = select_first(docs, lambda x: x.kind == kind)
+        if config is None:
+            return load_yaml_obj(types.DEFAULT, 'azure', kind)
+        return config        
 
