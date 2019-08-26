62f10e1c3c4ef68f30c6d16b473b760d4811131f
Luuk van Venrooij <luukvanvenrooij84@gmail.com>
2019-08-26 15:28:25 +0200

    - Added security group with subnet per component.

--- core/src/epicli/cli/engine/providers/azure/InfrastructureBuilder.py
+++ core/src/epicli/cli/engine/providers/azure/InfrastructureBuilder.py
@@ -23,6 +23,32 @@ class InfrastructureBuilder(Step):
         vnet = self.get_virtual_network()
         infrastructure.append(vnet)
 
+        for component_key, component_value in self.cluster_model.specification.components.items():
+            if component_value['count'] < 1:
+                continue
+
+            subnet_index = 0
+
+            #For now we just take the first subnet definition. Still need to research subnets spread over seperate AAZ`s
+            if ((component_value.subnets == None) or len(component_value.subnets) == 0):
+                raise Exception(f'No subnet definition for component: { component_key }')
+
+            subnet_definition = component_value.subnets[0]
+            availability_zone = subnet_definition['availability_zone']
+            self.logger.info(f'Ignoring availability-zone: { availability_zone } for subnet for component {component_key}')
+            
+            subnet = select_first(infrastructure, lambda item: item.kind == 'infrastructure/subnet' and
+                                    item.specification.address_prefix == subnet_definition['address_pool'])
+
+            if subnet is None:
+                security_group = self.get_security_group(component_key, subnet_index)
+                infrastructure.append(security_group)
+              
+                subnet = self.get_subnet(subnet_definition, component_key, security_group.specification.name, subnet_index)
+                infrastructure.append(subnet)
+                
+                subnet_index += 1
+
         return infrastructure
 
     def get_resource_group(self):
@@ -35,7 +61,20 @@ class InfrastructureBuilder(Step):
         vnet = self.get_config_or_default(self.docs, 'infrastructure/vnet')
         vnet.specification.name = resource_name(self.cluster_prefix, self.cluster_name, 'vnet')
         vnet.specification.address_space = self.cluster_model.specification.cloud.vnet_address_pool
-        return vnet            
+        return vnet
+
+    def get_security_group(self, component_key, index):
+        security_group = self.get_config_or_default(self.docs, 'infrastructure/security-group')
+        security_group.specification.name = resource_name(self.cluster_prefix, self.cluster_name, 'security-group' + '-' + str(index), component_key)
+        return security_group        
+
+    def get_subnet(self, subnet_definition, component_key, security_group_name, index):
+        subnet = self.get_config_or_default(self.docs, 'infrastructure/subnet')
+        subnet.specification.name = resource_name(self.cluster_prefix, self.cluster_name, 'subnet' + '-' + str(index), component_key)
+        subnet.specification.address_prefix = subnet_definition['address_pool']
+        subnet.specification.security_group_name = security_group_name
+        subnet.specification.cluster_name = self.cluster_name
+        return subnet                 
     
     @staticmethod
     def get_config_or_default(docs, kind):
