25d78c4ec3e224f702e14a539af6e925276de68a
Luuk van Venrooij <luukvanvenrooij84@gmail.com>
2019-08-27 13:31:21 +0200

    Addition of VM
    - Added VM defaults.
    - Added VM Terraform Jinja Template
    - Added VM to AzureInfraBuilder
    - Fixed missing module __init__ files

--- core/src/epicli/cli/engine/providers/azure/InfrastructureBuilder.py
+++ core/src/epicli/cli/engine/providers/azure/InfrastructureBuilder.py
@@ -1,8 +1,12 @@
+import os
+import uuid
+
 from cli.helpers.Step import Step
 from cli.helpers.naming_helpers import resource_name
 from cli.helpers.doc_list_helpers import select_single, select_all
 from cli.helpers.doc_list_helpers import select_first
 from cli.helpers.data_loader import load_yaml_obj, types
+from cli.helpers.config_merger import merge_with_defaults
 
 class InfrastructureBuilder(Step):
     def __init__(self, docs):
@@ -12,6 +16,8 @@ class InfrastructureBuilder(Step):
         self.cluster_prefix = self.cluster_model.specification.prefix.lower()
         self.resource_group_name = resource_name(self.cluster_prefix, self.cluster_name, 'rg')
         self.region = self.cluster_model.specification.cloud.region
+        # For linux we dont need a PW since we only support SSH so just add something random for now.
+        self.lnx_vm_pw = str(uuid.uuid4())
         self.docs = docs
 
     def run(self):
@@ -24,14 +30,16 @@ class InfrastructureBuilder(Step):
         infrastructure.append(vnet)
 
         for component_key, component_value in self.cluster_model.specification.components.items():
-            if component_value['count'] < 1:
+            vm_count = component_value['count']
+            if vm_count < 1:
                 continue
 
-            subnet_index = 0
-
-            #For now we just take the first subnet definition. Still need to research subnets spread over seperate AAZ`s
+            #TODO: For now we just take the first subnet definition. Still need to research subnets spread over seperate AAZ`s
             if ((component_value.subnets == None) or len(component_value.subnets) == 0):
                 raise Exception(f'No subnet definition for component: { component_key }')
+
+            if (len(component_value.subnets) > 1):
+                self.logger.warning(f'On Azure only one subnet per component is supported for now. Taking first and ignoring others.')               
 
             subnet_definition = component_value.subnets[0]
             availability_zone = subnet_definition['availability_zone']
@@ -41,13 +49,17 @@ class InfrastructureBuilder(Step):
                                     item.specification.address_prefix == subnet_definition['address_pool'])
 
             if subnet is None:
-                security_group = self.get_security_group(component_key, subnet_index)
+                security_group = self.get_security_group(component_key, 0)
                 infrastructure.append(security_group)
               
-                subnet = self.get_subnet(subnet_definition, component_key, security_group.specification.name, subnet_index)
+                subnet = self.get_subnet(subnet_definition, component_key, security_group.specification.name, 0)
                 infrastructure.append(subnet)
-                
-                subnet_index += 1
+
+            #TODO: For now we create the VM infrastructure compatible with the Epiphany 2.x 
+            #      code line but later we might want to look at scale sets to achieve the same result:
+            #      https://www.terraform.io/docs/providers/azurerm/r/virtual_machine_scale_set.html
+            for index in range(vm_count):
+                infrastructure.append(self.get_vm(component_key, component_value, index))
 
         return infrastructure
 
@@ -74,12 +86,40 @@ class InfrastructureBuilder(Step):
         subnet.specification.address_prefix = subnet_definition['address_pool']
         subnet.specification.security_group_name = security_group_name
         subnet.specification.cluster_name = self.cluster_name
-        return subnet                 
+        return subnet     
+
+    def get_vm(self, component_key, component_value, index):
+        vm = self.get_virtual_machine(component_value, self.cluster_model, self.docs)
+        vm.specification.name = resource_name(self.cluster_prefix, self.cluster_name, 'vm' + '-' + str(index), component_key)
+        vm.specification.admin_username = self.cluster_model.specification.admin_user.name
+        if vm.specification.os_type == 'linux':
+            # For linux we dont need a PW since we only support SSH so just add something random.
+            vm.specification.admin_password = self.lnx_vm_pw
+        if vm.specification.os_type == 'windows':
+            #TODO: We need PW or can we support SSH or something different on Windows?
+            vm.specification.admin_password = 'TODO' 
+        pub_key_path = self.cluster_model.specification.admin_user.key_path + '.pub'
+        if os.path.isfile(pub_key_path):
+            vm.specification.public_key = pub_key_path
+        else:
+            raise Exception(f'SSH key path "{pub_key_path}" is not valid. Ansible run will fail.')
+        return vm                
     
     @staticmethod
     def get_config_or_default(docs, kind):
         config = select_first(docs, lambda x: x.kind == kind)
         if config is None:
             return load_yaml_obj(types.DEFAULT, 'azure', kind)
-        return config        
+        return config   
+
+    @staticmethod
+    def get_virtual_machine(component_value, cluster_model, docs):
+        machine_selector = component_value.machine
+        model_with_defaults = select_first(docs, lambda x: x.kind == 'infrastructure/virtual-machine' and
+                                                                 x.name == machine_selector)
+        if model_with_defaults is None:
+            model_with_defaults = merge_with_defaults(cluster_model.provider, 'infrastructure/virtual-machine',
+                                                      machine_selector)
+
+        return model_with_defaults         
 
--- core/src/epicli/tests/cli/engine/providers/azure/test_AzureConfigBuilder.py
+++ core/src/epicli/tests/cli/engine/providers/azure/test_AzureConfigBuilder.py
@@ -23,6 +23,31 @@ def test_get_virtual_network_should_set_proper_values_to_model():
     assert actual.specification.address_space == '10.22.0.0/22'
 
 
+def test_get_security_group_should_set_proper_values_to_model():
+    cluster_model = get_cluster_model(cluster_name='TestCluster')
+    builder = InfrastructureBuilder([cluster_model])
+
+    actual = builder.get_security_group('component', 1)
+
+    assert actual.specification.name == 'prefix-testcluster-component-security-group-1'
+
+
+def test_get_subnet_should_set_proper_values_to_model():
+    cluster_model = get_cluster_model(cluster_name='TestCluster')
+    subnet_definition = dict_to_objdict({
+        'address_pool': '10.20.0.0/24',
+        'availability_zone': 'eu-west-2a'
+    })    
+    builder = InfrastructureBuilder([cluster_model])
+
+    actual = builder.get_subnet(subnet_definition, 'component', 'prefix-testcluster-component-security-group-1', 1)
+
+    assert actual.specification.name == 'prefix-testcluster-component-subnet-1'
+    assert actual.specification.address_prefix == subnet_definition['address_pool']
+    assert actual.specification.security_group_name == 'prefix-testcluster-component-security-group-1'
+    assert actual.specification.cluster_name == 'testcluster'
+
+
 def get_cluster_model(address_pool='10.22.0.0/22', cluster_name='EpiphanyTestCluster'):
     cluster_model = dict_to_objdict({
         'kind': 'epiphany-cluster',
