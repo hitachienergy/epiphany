f55b51e774c7b74551572c2ab10d41d274918fb3
Luuk van Venrooij <luukvanvenrooij84@gmail.com>
2019-09-03 13:25:14 +0200

    Multiple subnets per component:
    - Added multiple subnets per component
    - Added subnet_network_security_group_association for deprecated subnet link
    - Fixed tests

--- core/src/epicli/cli/engine/providers/azure/InfrastructureBuilder.py
+++ core/src/epicli/cli/engine/providers/azure/InfrastructureBuilder.py
@@ -1,5 +1,4 @@
 import os
-import uuid
 
 from cli.helpers.Step import Step
 from cli.helpers.naming_helpers import resource_name
@@ -16,8 +15,6 @@ class InfrastructureBuilder(Step):
         self.cluster_prefix = self.cluster_model.specification.prefix.lower()
         self.resource_group_name = resource_name(self.cluster_prefix, self.cluster_name, 'rg')
         self.region = self.cluster_model.specification.cloud.region
-        # For linux we dont need a PW since we only support SSH so just add something random for now.
-        self.lnx_vm_pw = str(uuid.uuid4())
         self.docs = docs
 
     def run(self):
@@ -34,32 +31,44 @@ class InfrastructureBuilder(Step):
             if vm_count < 1:
                 continue
 
-            #TODO: For now we just take the first subnet definition. Still need to research subnets spread over seperate AAZ`s
-            if ((component_value.subnets == None) or len(component_value.subnets) == 0):
-                raise Exception(f'No subnet definition for component: { component_key }')
+            subnets_to_create = []
+            security_groups_to_create = []
+            subnet_index = 0
 
-            if (len(component_value.subnets) > 1):
-                self.logger.warning(f'On Azure only one subnet per component is supported for now. Taking first and ignoring others.')               
+            #TODO: We support several subnets per component but still need to research subnets spread over seperate AAZ`s.
+            for subnet_definition in component_value.subnets:
+                subnet = select_first(infrastructure, lambda item: item.kind == 'infrastructure/subnet' and
+                                      item.specification.address_prefix == subnet_definition['address_pool'])
+                security_group = select_first(infrastructure, lambda item: item.kind == 'infrastructure/security-group' and
+                                              item.specification.address_prefix == subnet_definition['address_pool'])
 
-            subnet_definition = component_value.subnets[0]
-            availability_zone = subnet_definition['availability_zone']
-            self.logger.info(f'Ignoring availability-zone: { availability_zone } for subnet for component {component_key}')
-            
-            subnet = select_first(infrastructure, lambda item: item.kind == 'infrastructure/subnet' and
-                                    item.specification.address_prefix == subnet_definition['address_pool'])
+                if subnet is None:
+                    security_group = self.get_security_group(subnet_definition, component_key, subnet_index)
+                    infrastructure.append(security_group)
+                
+                    subnet = self.get_subnet(subnet_definition, component_key, subnet_index)
+                    infrastructure.append(subnet)
 
-            if subnet is None:
-                security_group = self.get_security_group(component_key, 0)
-                infrastructure.append(security_group)
-              
-                subnet = self.get_subnet(subnet_definition, component_key, security_group.specification.name, 0)
-                infrastructure.append(subnet)
+                    ssg_association = self.get_subnet_network_security_group_association(component_key, 
+                                                                                         subnet.specification.name, 
+                                                                                         security_group.specification.name,
+                                                                                         subnet_index)
+                    infrastructure.append(ssg_association)
+
+                    subnet_index += 1
+
+                subnets_to_create.append(subnet)
+                security_groups_to_create.append(security_group)    
 
             #TODO: For now we create the VM infrastructure compatible with the Epiphany 2.x 
             #      code line but later we might want to look at scale sets to achieve the same result:
             #      https://www.terraform.io/docs/providers/azurerm/r/virtual_machine_scale_set.html
-            for index in range(vm_count):
-                infrastructure.append(self.get_vm(component_key, component_value, index))
+            #for index in range(vm_count):
+            #
+            #    if self.cluster_model.specification.cloud.use_public_ips:
+            #        #TODO add public ip
+
+            #    infrastructure.append(self.get_vm(component_key, component_value, index))                               
 
         return infrastructure
 
@@ -75,18 +84,25 @@ class InfrastructureBuilder(Step):
         vnet.specification.address_space = self.cluster_model.specification.cloud.vnet_address_pool
         return vnet
 
-    def get_security_group(self, component_key, index):
+    def get_security_group(self, subnet_definition, component_key, index):
         security_group = self.get_config_or_default(self.docs, 'infrastructure/security-group')
         security_group.specification.name = resource_name(self.cluster_prefix, self.cluster_name, 'security-group' + '-' + str(index), component_key)
-        return security_group        
+        security_group.specification.address_prefix = subnet_definition['address_pool']
+        return security_group       
 
-    def get_subnet(self, subnet_definition, component_key, security_group_name, index):
+    def get_subnet(self, subnet_definition, component_key, index):
         subnet = self.get_config_or_default(self.docs, 'infrastructure/subnet')
         subnet.specification.name = resource_name(self.cluster_prefix, self.cluster_name, 'subnet' + '-' + str(index), component_key)
         subnet.specification.address_prefix = subnet_definition['address_pool']
-        subnet.specification.security_group_name = security_group_name
         subnet.specification.cluster_name = self.cluster_name
         return subnet     
+
+    def get_subnet_network_security_group_association(self, component_key, subnet_name, security_group_name, index):
+        ssg_association = self.get_config_or_default(self.docs, 'infrastructure/subnet-network-security-group-association')
+        ssg_association.specification.name = resource_name(self.cluster_prefix, self.cluster_name, 'ssg-association' + '-' + str(index), component_key)
+        ssg_association.specification.subnet_name = subnet_name
+        ssg_association.specification.security_group_name = security_group_name
+        return ssg_association
 
     def get_vm(self, component_key, component_value, index):
         vm = self.get_virtual_machine(component_value, self.cluster_model, self.docs)
@@ -94,7 +110,7 @@ class InfrastructureBuilder(Step):
         vm.specification.admin_username = self.cluster_model.specification.admin_user.name
         if vm.specification.os_type == 'linux':
             # For linux we dont need a PW since we only support SSH so just add something random.
-            vm.specification.admin_password = self.lnx_vm_pw
+            vm.specification.admin_password = "NeverGonnaNeed!"
         if vm.specification.os_type == 'windows':
             #TODO: We need PW or can we support SSH or something different on Windows?
             vm.specification.admin_password = 'TODO' 
