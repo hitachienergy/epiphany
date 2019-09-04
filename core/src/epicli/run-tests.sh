1abb4bbe55d12744830d96aff3b923ca077a1ba8
Przemek <przemyslaw.dyrda@pl.abb.com>
2019-09-04 16:29:54 +0200

    Changed ansible default 10s ping timeout to 60s; added junit xml reports for unit tests

--- core/src/epicli/run-tests.sh
+++ core/src/epicli/run-tests.sh
@@ -1,4 +1,4 @@
 # Run the python test for Epicli
 mkdir -p tests_result
-python -m pytest ./tests/ | tee tests_result/result.txt
+python -m pytest ./tests/ --junit-xml=tests_result/result.xml | tee tests_result/result.txt
 echo "Done running tests. See tests_result/result.txt"
