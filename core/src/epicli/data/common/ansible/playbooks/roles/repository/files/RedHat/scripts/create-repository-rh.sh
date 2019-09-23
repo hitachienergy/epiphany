cc00b038c8ceb3a5f6d358d3ce3465661c2a9bdf
erzetpe <erzetpe@gmail.com>
2019-09-23 17:47:03 +0200

    Add offline/online mode for Red Hat (#519)

--- core/src/epicli/data/common/ansible/playbooks/roles/repository/files/RedHat/scripts/create-repository-rh.sh
+++ core/src/epicli/data/common/ansible/playbooks/roles/repository/files/RedHat/scripts/create-repository-rh.sh
@@ -1,33 +1,21 @@
 #!/bin/bash
 
-PACKAGE_LIST=$(cat $1)
-LOG_FILE=/root/script-execution.log
+EPIPHANY_FILES_LOCATION=$1;
+OFFLINE_MODE=$2;
+LOG_FILE=/root/script-execution.log;
 
 WWW_SERVER_PATH=/var/www/html;
 
-REPOSITORY_PATH=$WWW_SERVER_PATH/repos;
-FILES_PATH=$WWW_SERVER_PATH/files;
-IMAGES_PATH=$WWW_SERVER_PATH/images;
+if $OFFLINE_MODE = true;
+then
+   rpm -i $EPIPHANY_FILES_LOCATION/offline_prereqs/*.rpm;
+else
+   yum install -y httpd createrepo yum-utils;
+fi
 
-mkdir -p $WWW_SERVER_PATH;
-mkdir -p $REPOSITORY_PATH;
-mkdir -p $FILES_PATH;
-mkdir -p $IMAGES_PATH;
-
-yum install -y httpd createrepo  yum-utils;
-
-for package in $PACKAGE_LIST ; do
-        echo "========== $package =========" | tee $LOG_FILE;
-        repoquery -a --qf  '%{ui_nevra}' $package;
-        repoquery -a --qf  '%{ui_nevra}' $package | xargs yumdownloader --destdir $REPOSITORY_PATH | tee $LOG_FILE;
-        echo "========== $package - dependencies =========" | tee $LOG_FILE;
-        repoquery -R --resolve -a --qf  '%{ui_nevra}' $package;
-        repoquery -R --resolve -a --qf  '%{ui_nevra}' $package | xargs yumdownloader --destdir $REPOSITORY_PATH | tee $LOG_FILE;
-done
+mv $EPIPHANY_FILES_LOCATION/* $WWW_SERVER_PATH;
 
 setenforce 0;
 systemctl start httpd;
 
 createrepo $REPOSITORY_PATH;
-
-
