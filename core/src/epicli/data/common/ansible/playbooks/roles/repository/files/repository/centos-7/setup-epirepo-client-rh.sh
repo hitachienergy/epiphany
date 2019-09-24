ddccb11dec2232cd0e7cdf789099c2987becb432
erzetpe <erzetpe@gmail.com>
2019-09-24 17:57:47 +0200

    Fixes for Red Hat script

--- core/src/epicli/data/common/ansible/playbooks/roles/repository/files/repository/centos-7/setup-epirepo-client-rh.sh
+++ core/src/epicli/data/common/ansible/playbooks/roles/repository/files/repository/centos-7/setup-epirepo-client-rh.sh
@@ -4,12 +4,13 @@ SERVER_IP=$1;
 
 curl -I -L $SERVER_IP/repos | grep "HTTP/1.1 200 OK";
 
-sed -i -e "s/enabled=1/enabled=0/g" /etc/yum.repos.d/*.repo;
+# Todo: remove if unnecessary
+#sed -i -e "s/enabled=1/enabled=0/g" /etc/yum.repos.d/*.repo;
 
 cat << EOF > /etc/yum.repos.d/epirepo.repo
 [epirepo]
 name=epirepo
-baseurl=http://$SERVER_IP/repos/
+baseurl=http://$SERVER_IP/packages/
 enabled=1
 gpgcheck=0
 EOF
--- core/src/epicli/data/common/ansible/playbooks/roles/repository/files/repository/redhat-7/create-repository-rh.sh
+++ core/src/epicli/data/common/ansible/playbooks/roles/repository/files/repository/redhat-7/create-repository-rh.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-EPIPHANY_FILES_LOCATION=$1;
-OFFLINE_MODE=$2;
+EPIPHANY_REQUIREMENTS_PATH=$1;
+IS_OFFLINE_MODE=$2;
 LOG_FILE=/root/script-execution.log;
 
 WWW_SERVER_PATH=/var/www/html;
 
-if $OFFLINE_MODE = true;
+REPOSITORY_PATH=$WWW_SERVER_PATH/packages;
+FILES_PATH=$WWW_SERVER_PATH/files;
+IMAGES_PATH=$WWW_SERVER_PATH/images;
+
+mkdir -p $REPOSITORY_PATH;
+mkdir -p $FILES_PATH;
+mkdir -p $IMAGES_PATH;
+
+if $IS_OFFLINE_MODE = true;
 then
-   rpm -i $EPIPHANY_FILES_LOCATION/offline_prereqs/*.rpm;
+   yum install -y `ls $EPIPHANY_REQUIREMENTS_PATH/packages/offline_prereqs/*.rpm`;
 else
    yum install -y httpd createrepo yum-utils;
 fi
 
-mv $EPIPHANY_FILES_LOCATION/* $WWW_SERVER_PATH;
+mv $EPIPHANY_REQUIREMENTS_PATH/* $WWW_SERVER_PATH;
 
 setenforce 0;
 systemctl start httpd;
