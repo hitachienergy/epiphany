0dd2ad49dc04943f39bcb1362db5fcaa23272da0
to-bar <46519524+to-bar@users.noreply.github.com>
2019-09-26 15:05:22 +0200

    Refactoring repository role (#532)
    - Refactoring repository role

--- core/src/epicli/data/common/ansible/playbooks/roles/repository/files/repository/RedHat/setup-epirepo-client.sh
+++ core/src/epicli/data/common/ansible/playbooks/roles/repository/files/repository/RedHat/setup-epirepo-client.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-SERVER_IP=$1;
+REPOSITORY_URL=$1
 
-curl -I -L $SERVER_IP/repos | grep "HTTP/1.1 200 OK";
+curl -I -L $REPOSITORY_URL | grep "HTTP/1.1 200 OK"
 
 # Todo: remove if unnecessary
 #sed -i -e "s/enabled=1/enabled=0/g" /etc/yum.repos.d/*.repo;
@@ -10,12 +10,11 @@ curl -I -L $SERVER_IP/repos | grep "HTTP/1.1 200 OK";
 cat << EOF > /etc/yum.repos.d/epirepo.repo
 [epirepo]
 name=epirepo
-baseurl=http://$SERVER_IP/packages/
+baseurl=$REPOSITORY_URL/packages/
 enabled=1
 gpgcheck=0
 EOF
 
-yum-config-manager --enable epirepo*;
-yum makecache;
-yum repolist;
-
+yum-config-manager --enable epirepo
+yum makecache
+yum repolist
