b465281c2857d782a005039656aeb534c61fcedd
Tomasz Szostak <tomasz.szostak@gmail.com>
2019-09-30 10:29:39 +0200

    Changed scripts fore Debian packages (#540)
    * Installing packages + files + images using artifacts repository
    
    * Prometheus, Grafana, Exporters, Kafka, Zookeeper to use private repo
    
    * Kafka exporter moved to private repo
    
    * Missing flannel image definition in K8s configuration
    
    * Download role cleanup
    
    * Install role cleanup

--- core/src/epicli/data/common/ansible/playbooks/roles/repository/files/prepare/ubuntu-18.04/download-requirements.sh
+++ core/src/epicli/data/common/ansible/playbooks/roles/repository/files/prepare/ubuntu-18.04/download-requirements.sh
@@ -1,2 +1,10 @@
-echo "placeholder"
+
 ./skopeo_linux --version
+
+./add-repositories.sh
+
+PACKAGE_LIST=$(cat ./requirements.txt)
+for package in $PACKAGE_LIST ; do
+        echo $package
+        apt-get install -y --download-only $package
+done
--- core/src/epicli/data/common/ansible/playbooks/roles/repository/files/repository/Debian/create-repository.sh
+++ core/src/epicli/data/common/ansible/playbooks/roles/repository/files/repository/Debian/create-repository.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 SERVER_IP=$1;
-DATE=`date +%Y.%m.%d-%H.%M.%S`;
 
-curl -I -L $SERVER_IP/repos | grep "HTTP/1.1 200 OK";
-
-cp /etc/apt/sources.list /etc/apt/sources.list.bak_$DATE;
-echo "deb [trusted=yes] http://$SERVER_IP/repos/ bionic main" > /etc/apt/sources.list;
+echo "deb [trusted=yes] http://$SERVER_IP/epirepo/ packages/" > /etc/apt/sources.list.d/epirepo.list;
 
 apt-cache policy;
 
