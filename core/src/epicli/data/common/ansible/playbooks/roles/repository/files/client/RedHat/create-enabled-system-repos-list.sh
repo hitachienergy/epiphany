d887b3d0a2f5ecbfe0ad0c032f916e46643f064c
to-bar <46519524+to-bar@users.noreply.github.com>
2019-10-07 12:30:33 +0200

    enabled-system-repos.txt in /var/tmp #536

--- core/src/epicli/data/common/ansible/playbooks/roles/repository/files/client/RedHat/create-enabled-system-repos-list.sh
+++ core/src/epicli/data/common/ansible/playbooks/roles/repository/files/client/RedHat/create-enabled-system-repos-list.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
-ENABLED_REPOS_FILE=/tmp/enabled-system-repos.txt
+ENABLED_REPOS_FILE=/var/tmp/enabled-system-repos.txt
 
 if [ ! -f "$ENABLED_REPOS_FILE" ]; then
   yum repolist -v enabled | grep -i Repo-id | awk -F ":" '{print $2}' |  sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | awk -F "/" '{print $1}' > $ENABLED_REPOS_FILE
