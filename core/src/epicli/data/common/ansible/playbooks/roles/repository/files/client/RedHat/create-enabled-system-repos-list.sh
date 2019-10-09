a853c1fe09d3150fbd589add5b883ff1c5d4e98b
toszo <tomasz.szostak@gmail.com>
2019-10-09 09:50:56 +0200

    Fixes for centos offline mode

--- core/src/epicli/data/common/ansible/playbooks/roles/repository/files/client/RedHat/create-enabled-system-repos-list.sh
+++ core/src/epicli/data/common/ansible/playbooks/roles/repository/files/client/RedHat/create-enabled-system-repos-list.sh
@@ -3,5 +3,5 @@
 ENABLED_REPOS_FILE=/var/tmp/enabled-system-repos.txt
 
 if [ ! -f "$ENABLED_REPOS_FILE" ]; then
-  yum repolist -v enabled | grep -i Repo-id | awk -F ":" '{print $2}' |  sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | awk -F "/" '{print $1}' > $ENABLED_REPOS_FILE
+  yum repolist enabled | awk '/^$/ {next}; /repo id/ {f=1; next}; /^repolist/ {f=0}; f {sub(/\/.*/,""); print $1}' > $ENABLED_REPOS_FILE
 fi
