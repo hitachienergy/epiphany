cc937822b273429b27a3cee10b836614dc6e8971
lukurde <47138492+lukurde@users.noreply.github.com>
2019-10-16 19:11:03 +0200

    agh

--- core/src/epicli/data/common/ansible/playbooks/roles/repository/files/server/Debian/create-repository.sh
+++ core/src/epicli/data/common/ansible/playbooks/roles/repository/files/server/Debian/create-repository.sh
@@ -2,18 +2,23 @@
 
 EPI_REPO_SERVER_PATH=$1 # /var/www/html/epirepo is the default
 IS_OFFLINE_MODE=$2
+script_path="$( cd "$(dirname "$0")" ; pwd -P )"
 
 if $IS_OFFLINE_MODE = true; then
     # bootstrap apache and dpkg-dev installation in air-gap mode
-    mv /etc/apt/sources.list /etc/apt/sources.list.bak
+    if [[ -f /etc/apt/sources.list ]]; then
+        echo "disabling default repositories..."
+        mv /etc/apt/sources.list /etc/apt/sources.list.bak
+    fi
     if ! dpkg -l | grep -q libdpkg-perl; then
         echo libdpkg-perl not found, installing...
         dpkg -i "${EPI_REPO_SERVER_PATH}"/packages/libdpkg-perl*.deb
     fi
-    cd "${EPI_REPO_SERVER_PATH}"/packages && /tmp/epi-repository-setup-scripts/dpkg-scanpackages -m . | gzip -9c > Packages.gz && cd -
+    echo "generating repository metadata..."
+    cd "${EPI_REPO_SERVER_PATH}"/packages && /tmp/epi-repository-setup-scripts/dpkg-scanpackages -m . | gzip -9c > Packages.gz && cd "${script_path}"
     echo "deb [trusted=yes] file:${EPI_REPO_SERVER_PATH}/packages ./" > /etc/apt/sources.list.d/epilocal.list
     #apt update --assume-no # workaround for botched docker repository https://github.com/docker/for-linux/issues/812
-    echo "updating apt..."
+    echo "updating apt and installing apache..."
     apt -y update
     apt -y install apache2 dpkg-dev
     echo "removing temporary repo definition: /etc/apt/sources.list.d/epilocal.list..."
@@ -28,7 +33,7 @@ else
     # -m is important because it allow same packages with different versions
     # 'cd' is needed here becuase 'dpkg-scanpackages' prepends path to "Filename" field in Packages.gz
     # otherwise it would break package URL for apt
-    cd /var/www/html/epirepo/packages && dpkg-scanpackages -m . | gzip -9c > Packages.gz
+    cd /var/www/html/epirepo/packages && dpkg-scanpackages -m . | gzip -9c > Packages.gz && cd "${script_path}"
 fi
 
 systemctl start apache2
