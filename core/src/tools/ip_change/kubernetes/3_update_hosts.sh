982c798eaeb304bfba0feb98d51f3e4f27f3e626
Tomasz Szostak <tomasz.szostak@gmail.com>
2019-10-23 13:56:04 +0200

    Update core/src/tools/ip_change/kubernetes/3_update_hosts.sh
    Co-Authored-By: erzetpe <erzetpe@gmail.com>

--- core/src/tools/ip_change/kubernetes/3_update_hosts.sh
+++ core/src/tools/ip_change/kubernetes/3_update_hosts.sh
@@ -19,6 +19,6 @@ NEW_IP=$2
 
 echo "==== Modifying /etc/hosts for new IP ===="
 
-sed -i "s/$OLD_IP/$NEW_IP/" "/etc/hosts"
+sed -i "s/$OLD_IP/$NEW_IP/g" "/etc/hosts"
 
 echo "==== /etc/hosts modification completed ===="
