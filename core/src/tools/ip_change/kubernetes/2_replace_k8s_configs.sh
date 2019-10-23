82cf5844df2c46084b1ca39f29115c28ab3bc8f9
Tomasz Szostak <tomasz.szostak@gmail.com>
2019-10-23 14:07:09 +0200

    Update core/src/tools/ip_change/kubernetes/2_replace_k8s_configs.sh
    Co-Authored-By: erzetpe <erzetpe@gmail.com>

--- core/src/tools/ip_change/kubernetes/2_replace_k8s_configs.sh
+++ core/src/tools/ip_change/kubernetes/2_replace_k8s_configs.sh
@@ -21,7 +21,7 @@ echo "==== Modifying Kubernetes manifests ===="
 
 find /etc/kubernetes/ -type f | xargs grep $OLD_IP
 
-find /etc/kubernetes/ -type f | xargs sed -i "s/$OLD_IP/$NEW_IP/"
+find /etc/kubernetes/ -type f | xargs sed -i "s/$OLD_IP/$NEW_IP/g"
 
 find /etc/kubernetes/ -type f | xargs grep $NEW_IP
 
