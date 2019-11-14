b1e0a7ff7e832fd0ee962adfc380885c4d9543b5
lukurde <47138492+lukurde@users.noreply.github.com>
2019-11-14 18:50:28 +0100

    whoops 2: electric boogaloo

--- core/src/epicli/data/common/ansible/playbooks/roles/repository/files/download-requirements/ubuntu-18.04/download-requirements.sh
+++ core/src/epicli/data/common/ansible/playbooks/roles/repository/files/download-requirements/ubuntu-18.04/download-requirements.sh
@@ -56,9 +56,11 @@ mkdir -p "${dst_dir_files}"
 mkdir -p "${dst_dir_images}"
 
 # mask custom repositories to avoid possible conflicts
+shopt -s nullglob
 for i in /etc/apt/sources.list.d/*.list; do
     mv "${i}" "${i}.bak"
 done
+shopt -u nullglob
 
 # add 3rd party repositories
 . ${add_repos} 
@@ -158,6 +160,8 @@ for i in $(grep -o '[[:blank:]]/etc/apt/sources.list.d/.*list' ${add_repos}); do
 done
 
 # restore masked custom repositories to their original names
+shopt -s nullglob
 for i in /etc/apt/sources.list.d/*.list.bak; do
     mv "${i}" "${i::-4}"
 done
+shopt -u nullglob
