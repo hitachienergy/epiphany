3459723e4b2b35e5580aff50c6a4d5131fa32128
Luuk van Venrooij <luukvanvenrooij84@gmail.com>
2020-02-12 07:54:53 +0100

    Minor improvement to cert config script.

--- core/src/epicli/.devcontainer/cert/config.sh
+++ core/src/epicli/.devcontainer/cert/config.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 FILES=/usr/local/share/ca-certificates/*.crt
-for f in $FILES
-do
-  echo "Processing cert $f..."
-  chmod 644 $f
-  pip config set global.cert $f
-done
-pip config list
-update-ca-certificates
+
+if [ ${#FILES[@]} -gt 0 ]
+then
+  echo "Processing cert ${FILES[0]}..."
+  chmod 644 ${FILES[0]}
+  pip config set global.cert ${FILES[0]}
+  pip config list
+  update-ca-certificates  
+fi
+
