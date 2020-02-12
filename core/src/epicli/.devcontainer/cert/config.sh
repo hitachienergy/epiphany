fc6023b78e5f2199e84c42e735c971cfadbcda18
Luuk van Venrooij <luukvanvenrooij84@gmail.com>
2020-02-12 11:39:05 +0100

    Minor fixes for container without cert.

--- core/src/epicli/.devcontainer/cert/config.sh
+++ core/src/epicli/.devcontainer/cert/config.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-FILES=/usr/local/share/ca-certificates/*.crt
-if [ ${#FILES[@]} -gt 0 ]
-then
-  echo "Processing cert ${FILES[0]}..."
-  chmod 644 ${FILES[0]}
-  pip config set global.cert ${FILES[0]}
+if ls /usr/local/share/ca-certificates/*.crt 1> /dev/null 2>&1; then
+  pattern="/usr/local/share/ca-certificates/*.crt"
+  files=( $pattern )
+  f="${files[0]}"
+  echo "Processing cert $f"
+  chmod 644 $f
+  pip config set global.cert $f
   pip config list
   update-ca-certificates  
+else
+     echo "No cert to setup"
 fi
 
