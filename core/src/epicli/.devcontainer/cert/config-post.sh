b86d3ea4e217efce451ea964645474f1b7a0b6f5
Luuk van Venrooij <luukvanvenrooij84@gmail.com>
2020-02-20 23:04:03 +0100

    Setup cert for Ansible and AWS Boto3.

--- core/src/epicli/.devcontainer/cert/config-post.sh
+++ core/src/epicli/.devcontainer/cert/config-post.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 if ls /usr/local/share/ca-certificates/*.crt 1> /dev/null 2>&1; then
+  echo "Setup cert for Ansible, AWS..."
   pattern="/usr/local/share/ca-certificates/*.crt"
   files=( $pattern )
   f="${files[0]}"
-  echo "Processing cert $f"
-  chmod 644 $f
-  pip config set global.cert $f
-  pip config list
-  update-ca-certificates  
-  echo -e "AWS_CA_BUNDLE=$f\nANSIBLE_TIMEOUT=0" >> /etc/environment
+  mkdir "/home/vscode/.aws/"
+  echo -e "[defaults]\ntimeout=0" >> "/home/vscode/.ansible.cfg"
+  echo -e "[default]\nca_bundle=$f" >> "/home/vscode/.aws/config"
 else
      echo "No cert to setup"
 fi
