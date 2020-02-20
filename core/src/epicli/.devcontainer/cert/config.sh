52d6fa9d321dbac7c8c601fe2ab52e9fd610c4fb
Luuk van Venrooij <luukvanvenrooij84@gmail.com>
2020-02-20 15:25:52 +0100

    Set cert path for boto and set timeout for Ansible SSH to 0

--- core/src/epicli/.devcontainer/cert/config.sh
+++ core/src/epicli/.devcontainer/cert/config.sh
@@ -8,6 +8,7 @@ if ls /usr/local/share/ca-certificates/*.crt 1> /dev/null 2>&1; then
   pip config set global.cert $f
   pip config list
   update-ca-certificates  
+  echo -e "AWS_CA_BUNDLE=$f\nANSIBLE_TIMEOUT=0" >> /etc/environment
 else
      echo "No cert to setup"
 fi
