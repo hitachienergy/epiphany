fbb0b3fe29e94e91a8c49b13831ef5618162cbd6
Luuk van Venrooij <luukvanvenrooij84@gmail.com>
2020-02-25 16:36:13 +0100

    Fixes for init:
    - Added defaults for VMs
    - Removal off SET_BY_AUTOMATION

--- core/src/epicli/cli/engine/InitEngine.py
+++ core/src/epicli/cli/engine/InitEngine.py
@@ -1,10 +1,12 @@
 
 import os
+import sys
 
 from cli.helpers.Step import Step
 from cli.helpers.build_saver import save_manifest, get_build_path
 from cli.helpers.data_loader import load_all_yaml_objs, types, load_all_documents_from_folder
 from cli.engine.BuildEngine import BuildEngine
+from cli.helpers.objdict_helpers import remove_value
 from cli.version import VERSION
 
 
@@ -41,14 +43,30 @@ class InitEngine(Step):
     def get_full_config(self, config_docs):
         cluster_config_path = save_manifest(config_docs, self.name, self.name + '.yml')
         args = type('obj', (object,), {'file': cluster_config_path})()
+
+        # generate the feature documents
         with BuildEngine(args) as build:
             docs = build.dry_run()
+
+        # VMs are curently the infrastructure documents the user might interact with for:
+        # - type/size
+        # - distro
+        # - network security rules
+        # ...
+        # So we add the defaults here.
+        # TODO: Check if we want to include possible other infrastructure documents.
+        if self.provider != 'any':
+            vms = load_all_yaml_objs(types.DEFAULT, self.provider, 'infrastructure/virtual-machine')
+            docs = [*docs, *vms]
 
         # set the provider for all docs
         for doc in docs:
             if 'provider' not in doc.keys():
                 doc['provider'] = self.provider
 
+        # remove SET_BY_AUTOMATION fields
+        remove_value(docs, 'SET_BY_AUTOMATION')                
+        
         return docs
 
 
