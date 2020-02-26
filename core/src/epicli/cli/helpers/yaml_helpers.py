3c182aa37db207b64fde3d6dabaa1d002219bb85
Luuk van Venrooij <luukvanvenrooij84@gmail.com>
2020-02-26 13:08:39 +0100

    Changed library for YAML so comments are preserved
    - Introduced ruamel.yaml

--- core/src/epicli/cli/helpers/yaml_helpers.py
+++ core/src/epicli/cli/helpers/yaml_helpers.py
@@ -1,9 +1,11 @@
-import yaml
+from ruamel.yaml import YAML
 from cli.helpers.objdict_helpers import objdict_to_dict, dict_to_objdict
 
 
 def safe_load_all(stream):
-    docs = list(yaml.safe_load_all(stream))
+    yaml = YAML()
+    yaml.default_flow_style = False     
+    docs = list(yaml.load_all(stream))
     conv_docs = []
     for doc in docs:
         conv_docs.append(dict_to_objdict(doc))
@@ -11,18 +13,24 @@ def safe_load_all(stream):
 
 
 def safe_load(stream):
-    doc = yaml.safe_load(stream)
+    yaml = YAML()
+    yaml.default_flow_style = False
+    doc = yaml.load(stream)
     return dict_to_objdict(doc)
 
 
 def dump_all(docs, stream):
+    yaml = YAML()
+    yaml.default_flow_style = False    
     doc2 = docs
     conv_docs = []
     for doc in doc2:
         conv_docs.append(objdict_to_dict(doc))
-    yaml.dump_all(conv_docs, stream, default_flow_style=False)
+    yaml.dump_all(conv_docs, stream)
 
 
 def dump(doc, stream):
-    yaml.dump(objdict_to_dict(doc), stream, default_flow_style=False)
+    yaml = YAML()
+    yaml.default_flow_style = False
+    yaml.dump(objdict_to_dict(doc), stream)
 
