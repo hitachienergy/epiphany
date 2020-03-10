8d4b4fdb45b637abe5b80c356aed69fb87df53b1
Michal Opala <sk4zuzu@gmail.com>
2020-03-11 00:39:25 +0100

    cli: reducing sizes of repository and image_registry groups

--- core/src/epicli/cli/engine/ansible/AnsibleInventoryCreator.py
+++ core/src/epicli/cli/engine/ansible/AnsibleInventoryCreator.py
@@ -26,6 +26,9 @@ class AnsibleInventoryCreator(Step):
         inventory = self.get_inventory()
         save_inventory(inventory, self.cluster_model)
 
+    def should_be_singleton(self, ansible_role_name):
+        return ansible_role_name in {"image_registry", "repository"}
+
     def get_inventory(self):
         inventory = []
         for component_key, component_value in self.cluster_model.specification.components.items():
@@ -36,7 +39,11 @@ class AnsibleInventoryCreator(Step):
                 roles = self.get_roles_for_feature(component_key)
                 for role in roles:
                     ansible_role_name = to_role_name(role)
-                    inventory.append(AnsibleInventoryItem(ansible_role_name, ips))
+                    if self.should_be_singleton(ansible_role_name):
+                        effective_ips = ips[:1]
+                    else:
+                        effective_ips = ips
+                    inventory.append(AnsibleInventoryItem(ansible_role_name, effective_ips))
 
         return self.group_duplicated(inventory)
 
