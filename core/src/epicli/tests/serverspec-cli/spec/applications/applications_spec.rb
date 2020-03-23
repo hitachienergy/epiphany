09e4ca93c420f1b20ba9fbd248aff95b504408eb
Przemyslaw Dyrda <przemyslaw.dyrda@pl.abb.com>
2020-03-23 16:10:11 +0100

    Added spec tests for checking taints on master node in single machine installation

--- core/src/epicli/tests/serverspec-cli/spec/applications/applications_spec.rb
+++ core/src/epicli/tests/serverspec-cli/spec/applications/applications_spec.rb
@@ -4,14 +4,14 @@ require 'applications/rabbitmq/rabbitmq'
 require 'applications/auth-service/auth-service'
 require 'applications/ignite-stateless/ignite-stateless'
 
-if !readDataYaml("configuration/applications")["specification"]["applications"].select {|i| i["name"] == 'rabbitmq' }.nil?
+if !readDataYaml("configuration/applications")["specification"]["applications"].select {|i| i["name"] == 'rabbitmq' }.empty?
   callRabbitMQDeploymentTests
 end
 
-if !readDataYaml("configuration/applications")["specification"]["applications"].select {|i| i["name"] == 'auth-service' }.nil?
+if !readDataYaml("configuration/applications")["specification"]["applications"].select {|i| i["name"] == 'auth-service' }.empty?
   callAuthServiceDeploymentTests
 end
 
-if !readDataYaml("configuration/applications")["specification"]["applications"].select {|i| i["name"] == 'ignite-stateless' }.nil?
+if !readDataYaml("configuration/applications")["specification"]["applications"].select {|i| i["name"] == 'ignite-stateless' }.empty?
   callIgniteDeploymentTests
 end
