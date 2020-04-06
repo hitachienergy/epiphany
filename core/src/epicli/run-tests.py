aa471701b265fb47ee658d1a32c172e74a8cd8b6
Luuk van Venrooij <luukvanvenrooij84@gmail.com>
2020-04-06 10:42:07 +0200

    Cleaning up running of Python tests.

--- core/src/epicli/run-tests.py
+++ core/src/epicli/run-tests.py
@@ -1,31 +1,8 @@
 #!/usr/bin/env py
-import sys
-import argparse
-import json
-import os
 import subprocess
 
 def main():
-    parser = argparse.ArgumentParser(
-        description=__doc__,
-        usage='''test <command> [<args>]''',
-        formatter_class=argparse.RawDescriptionHelpFormatter)
-
-    subparsers = parser.add_subparsers()
-    python_parser(subparsers)
-
-    arg = parser.parse_args()
-    return arg.func(arg)
-
-
-def python_parser(subparsers):
-    sub_parser = subparsers.add_parser('python', description='Runs the Epicli python unit tests')
-
-    def run_python(args):
-        subprocess.call('mkdir -p tests/cli/results/ && python -m pytest ./tests/ --junit-xml=tests/cli/results/result.xml | tee tests/cli/results/result.txt', shell=True)
-
-    sub_parser.set_defaults(func=run_python)
-
+    return subprocess.call('mkdir -p tests/cli/results/ && python -m pytest ./tests/ --junit-xml=tests/cli/results/result.xml | tee tests/cli/results/result.txt', shell=True)
 
 if __name__ == '__main__':
     exit(main())
