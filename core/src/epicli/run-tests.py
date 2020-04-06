871011807608b064207bb62c2a60c3ef526eb4aa
Luuk van Venrooij <luukvanvenrooij84@gmail.com>
2020-04-06 22:12:48 +0200

    Re-structure Python unit tests.

--- core/src/epicli/run-tests.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env py
-import subprocess
-
-def main():
-    return subprocess.call('mkdir -p tests/cli/results/ && python -m pytest ./tests/ --junit-xml=tests/cli/results/result.xml | tee tests/cli/results/result.txt', shell=True)
-
-if __name__ == '__main__':
-    exit(main())
