8b385331bc9cc13073ce2b983707b444f674c266
Michał Opala <sk4zuzu@gmail.com>
2020-05-08 12:49:00 +0200

    backup/recovery: uncommenting and refactoring legacy code (#1241)

--- core/src/epicli/cli/engine/PatchEngine.py
+++ core/src/epicli/cli/engine/PatchEngine.py
@@ -11,6 +11,7 @@ class PatchEngine(Step):
     def __init__(self, input_data):
         super().__init__(__name__)
         self.build_directory = input_data.build_directory
+        self.components = input_data.components
         self.ansible_command = AnsibleCommand()
 
     def __enter__(self):
@@ -21,15 +22,17 @@ class PatchEngine(Step):
         super().__exit__(exc_type, exc_value, traceback)
 
     def backup(self):
-        self.upgrade_patch_files_and_run('backup')
+        for component in sorted(self.components):
+            self.upgrade_patch_files_and_run('backup', component)
         return 0
 
     def recovery(self):
-        self.upgrade_patch_files_and_run('recovery')
+        for component in sorted(self.components):
+            self.upgrade_patch_files_and_run('recovery', component)
         return 0
 
-    def upgrade_patch_files_and_run(self, action):
-        self.logger.info(f'Running {action}...')
+    def upgrade_patch_files_and_run(self, action, component):
+        self.logger.info(f'Running {action} on {component}...')
 
         #copy role files
         roles_build_path = os.path.join(self.build_directory, 'ansible/roles', action)
@@ -37,10 +40,10 @@ class PatchEngine(Step):
         copy_files_recursively(roles_source_path, roles_build_path)
 
         #copy playbook file
-        playbook_build_path = os.path.join(self.build_directory, 'ansible/') + action + '.yml'
-        playbook_source_path = os.path.join(AnsibleRunner.ANSIBLE_PLAYBOOKS_PATH) + action + '.yml' 
-        copy_file(playbook_source_path, playbook_build_path)  
-        
+        playbook_build_path = os.path.join(self.build_directory, 'ansible/') + action + '_' + component + '.yml'
+        playbook_source_path = os.path.join(AnsibleRunner.ANSIBLE_PLAYBOOKS_PATH) + action + '_' + component + '.yml'
+        copy_file(playbook_source_path, playbook_build_path)
+
         #run the playbook
         inventory_path = get_inventory_path_for_build(self.build_directory)
-        self.ansible_command.run_playbook(inventory=inventory_path, playbook_path=playbook_build_path)        
+        self.ansible_command.run_playbook(inventory=inventory_path, playbook_path=playbook_build_path)
