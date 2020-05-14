ab76e310bd84ecb8775146ed2bda5da386b5feea
erzetpe <erzetpe@gmail.com>
2020-05-14 13:51:32 +0200

    Fixes in configure-vault.sh after review

--- core/src/epicli/data/common/ansible/playbooks/roles/vault/files/configure-vault.sh
+++ core/src/epicli/data/common/ansible/playbooks/roles/vault/files/configure-vault.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Description: This script perform configuration of Hashicorp Vault to be used with Epiphany
+# More information you can find in Epiphany documentation in HOWTO.md
 # TODO: Revoke root token
 # TODO: Policy for non-root access
 
@@ -7,75 +9,79 @@ HELP_MESSAGE="Usage: configure-vault.sh -c path_to_script_configuration_file -a 
 function print_help { echo "$HELP_MESSAGE"; }
 
 function log_and_print {
-    local STRING_TO_LOG="$1";
-    echo "$STRING_TO_LOG" | tee -a configure-vault.log;
+    local string_to_log="$1";
+    echo "$string_to_log" | tee -a configure-vault.log;
 }
 
 function fail_script {
-    local STRING_TO_LOG="$1";
-    log_and_print "$STRING_TO_LOG";
+    local string_to_log="$1";
+    log_and_print "$string_to_log";
     exit 1;
 }
 
-function fail_on_vault_error {
-    local STATUS="$1";
-    local SUCCESS_MESSAGE="$2";
-    local FAILURE_MESSAGE="$3";
-    if [ "$1" != "1" ] ; then
-        log_and_print "$SUCCESS_MESSAGE";
+function check_vault_error {
+    local status="$1";
+    local success_message="$2";
+    local failure_message="$3";
+    if [ "$status" != "1" ] ; then
+        log_and_print "$success_message";
     else
-        fail_script "$FAILURE_MESSAGE";
+        fail_script "$failure_message";
     fi
 }
 
-function vault_unseal {
-  local INIT_FILE_PATH="$1";
-  log_and_print "Unsealing vault.";
-  grep -m 3 Unseal "$INIT_FILE_PATH" | awk '{print $4}' | while read -r line ; do
+function unseal_vault {
+  local init_file_path="$1";
+  log_and_print "Unsealing Vault.";
+  grep -m 3 Unseal "$init_file_path" | awk '{print $4}' | while read -r line ; do
     vault operator unseal "$line";
-    fail_on_vault_error "$?" "Unseal performed." "There was an error during unsealing of vault.";
+    check_vault_error "$?" "Unseal performed." "There was an error during unsealing of Vault.";
   done
 }
 
 function enable_auditing {
-    local LOG_DIR="$1";
+    local log_dir="$1";
     log_and_print "Enabling auditing.";
     vault audit list | grep "file";
-    COMMAND_RESULT=(${PIPESTATUS[@]})
+    local COMMAND_RESULT=(${PIPESTATUS[@]})
     if [ "$COMMAND_RESULT[0]" = "1"] ; then
         fail_script "There was an error during listing auditing.";
     fi
     if [ "$COMMAND_RESULT[1]" = "1" ] ; then
-        vault audit enable file file_path="$LOG_DIR/audit.log";
-        fail_on_vault_error "$?" "Auditing enabled." "There was an error during enabling auditing.";
+        vault audit enable file file_path="$log_dir/audit.log";
+        check_vault_error "$?" "Auditing enabled." "There was an error during enabling auditing.";
     fi
 }
 
 function mount_secret_path {
-    local SECRET_PATH="$1";
-    log_and_print "Mounting secret engine.";
-    vault secrets list | grep "$SECRET_PATH/";
-    COMMAND_RESULT=(${PIPESTATUS[@]})
+    local secret_path="$1";
+    log_and_print "Mounting secret engine...";
+    vault secrets list | grep "$secret_path/";
+    local COMMAND_RESULT=(${PIPESTATUS[@]})
     if [ "$COMMAND_RESULT[0]" = "1" ] ; then
         fail_script "There was an error during listing secret engines.";
     fi
     if [ "$COMMAND_RESULT[1]" = "1" ] ; then
-        vault secrets enable -path="$SECRET_PATH" kv-v2;
-        fail_on_vault_error "$?" "Secret engine enabled under path: $SECRET_PATH." "There was an error during enabling secret engine under path: $SECRET_PATH.";
+        vault secrets enable -path="$secret_path" -version=2 kv;
+        check_vault_error "$?" "Secret engine enabled under path: $secret_path." "There was an error during enabling secret engine under path: $secret_path.";
     fi
 }
 
-function integrate_kubernetes {
+function integrate_with_kubernetes {
     log_and_print "Turning on kubernetes integration.";
     vault auth list | grep kubernetes;
-    COMMAND_RESULT=(${PIPESTATUS[@]})
+    local COMMAND_RESULT=(${PIPESTATUS[@]})
     if [ "$COMMAND_RESULT[0]" = "1" ] ; then
         fail_script "There was an error during listing authentication methods.";
     fi
     if [ "$COMMAND_RESULT[1]" = "1" ] ; then
         vault auth enable kubernetes;
-        fail_on_vault_error "$?" "Kubernetes authentication enabled." "There was an error during enabling kubernetes authentication.";
+        check_vault_error "$?" "Kubernetes authentication enabled." "There was an error during enabling kubernetes authentication.";
     fi
+}
+
+function cleanup {
+    [ -e "$HOME/.vault-token" ] && rm -f "$HOME/.vault-token";
 }
 
 while getopts ":p:a:c:h?" opt; do
@@ -93,30 +99,32 @@ fi
 
 . "$CONFIG_FILE";
 
-INIT_FILE_PATH="$VAULT_INSTALL_PATH/init.txt"
+init_file_path="$VAULT_INSTALL_PATH/init.txt"
 export VAULT_ADDR="http://$VAULT_IP:8200"
 PATH=$VAULT_INSTALL_PATH/bin:$PATH
 
-trap "rm -f $HOME/.vault-token" EXIT;
+trap cleanup EXIT;
+trap cleanup INT;
+trap cleanup TERM;
 
 if [ "${SCRIPT_AUTO_UNSEAL,,}" = "true" ] ; then
-    vault_unseal "$INIT_FILE_PATH";
+    unseal_vault "$init_file_path";
 fi
 
-log_and_print "Loging to vault.";
-LOGIN_TOKEN="$(grep "Initial Root Token:" "$INIT_FILE_PATH" | awk -F'[ ]' '{print $4}')";
+log_and_print "Logging into Vault.";
+LOGIN_TOKEN="$(grep "Initial Root Token:" "$init_file_path" | awk -F'[ ]' '{print $4}')";
 vault login -no-print "$LOGIN_TOKEN";
-fail_on_vault_error "$?" "Login successful." "There was an error loging to vault.";
+check_vault_error "$?" "Login successful." "There was an error loging into Vault.";
 LOGIN_TOKEN="";
 
 if [ "${ENABLE_AUDITING,,}" = "true" ] ; then
-    enable_auditing "$LOG_DIR";
+    enable_auditing "$log_dir";
 fi
 
-mount_secret_path "$SECRET_PATH";
+mount_secret_path "$secret_path";
 
 if [ "${KUBERNETES_INTEGRATION,,}" = "true" ] ; then
-    integrate_kubernetes;
+    integrate_with_kubernetes;
 fi
 
 exit 0;
