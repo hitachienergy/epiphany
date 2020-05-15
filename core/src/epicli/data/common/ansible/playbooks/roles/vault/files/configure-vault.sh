09a5bd4c3163d3850da4cf4ee3091a3a7d81f08c
erzetpe <erzetpe@gmail.com>
2020-05-15 15:19:01 +0200

    Fixes after review, replace handler with conditional restart

--- core/src/epicli/data/common/ansible/playbooks/roles/vault/files/configure-vault.sh
+++ core/src/epicli/data/common/ansible/playbooks/roles/vault/files/configure-vault.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Description: This script perform configuration of Hashicorp Vault to be used with Epiphany
-# More information you can find in Epiphany documentation in HOWTO.md
+# Description: This script configures of Hashicorp Vault to be used with Epiphany
+# You can find more information in Epiphany documentation in HOWTO.md
 # TODO: Revoke root token
 # TODO: Policy for non-root access
 
@@ -15,7 +15,7 @@ function log_and_print {
 
 function exit_with_error {
     local string_to_log="$1";
-    log_and_print "$string_to_log";
+    log_and_print "ERROR: $string_to_log";
     exit 1;
 }
 
@@ -33,7 +33,7 @@ function check_vault_error {
 function unseal_vault {
   local init_file_path="$1";
   log_and_print "Unsealing Vault.";
-  grep -m 3 Unseal "$init_file_path" | awk '{print $4}' | while read -r line ; do
+  grep --max-count=3 Unseal "$init_file_path" | awk '{print $4}' | while read -r line ; do
     vault operator unseal "$line";
     check_vault_error "$?" "Unseal performed." "There was an error during unsealing of Vault.";
   done
@@ -56,11 +56,11 @@ function mount_secret_path {
     local secret_path="$1";
     log_and_print "Mounting secret engine...";
     vault secrets list | grep "$secret_path/";
-    local COMMAND_RESULT=(${PIPESTATUS[@]})
-    if [ "$COMMAND_RESULT[0]" = "1" ] ; then
+    local command_result=(${PIPESTATUS[@]})
+    if [ "$command_result[0]" = "1" ] ; then
         exit_with_error "There was an error during listing secret engines.";
     fi
-    if [ "$COMMAND_RESULT[1]" = "1" ] ; then
+    if [ "$command_result[1]" = "1" ] ; then
         vault secrets enable -path="$secret_path" -version=2 kv;
         check_vault_error "$?" "Secret engine enabled under path: $secret_path." "There was an error during enabling secret engine under path: $secret_path.";
     fi
@@ -69,11 +69,11 @@ function mount_secret_path {
 function integrate_with_kubernetes {
     log_and_print "Turning on Kubernetes integration.";
     vault auth list | grep kubernetes;
-    local COMMAND_RESULT=(${PIPESTATUS[@]})
-    if [ "$COMMAND_RESULT[0]" = "1" ] ; then
+    local command_result=(${PIPESTATUS[@]})
+    if [ "$command_result[0]" = "1" ] ; then
         exit_with_error "There was an error during listing authentication methods.";
     fi
-    if [ "$COMMAND_RESULT[1]" = "1" ] ; then
+    if [ "$command_result[1]" = "1" ] ; then
         vault auth enable kubernetes;
         check_vault_error "$?" "Kubernetes authentication enabled." "There was an error during enabling Kubernetes authentication.";
     fi
@@ -98,20 +98,20 @@ fi
 
 . "$CONFIG_FILE";
 
-init_file_path="$VAULT_INSTALL_PATH/init.txt"
+INIT_FILE_PATH="$VAULT_INSTALL_PATH/init.txt"
 export VAULT_ADDR="http://$VAULT_IP:8200"
 PATH=$VAULT_INSTALL_PATH/bin:$PATH
 
 trap cleanup EXIT INT TERM;
 
 if [ "${SCRIPT_AUTO_UNSEAL,,}" = "true" ] ; then
-    unseal_vault "$init_file_path";
+    unseal_vault "$INIT_FILE_PATH";
 fi
 
 log_and_print "Logging into Vault.";
-LOGIN_TOKEN="$(grep "Initial Root Token:" "$init_file_path" | awk -F'[ ]' '{print $4}')";
+LOGIN_TOKEN="$(grep "Initial Root Token:" "$INIT_FILE_PATH" | awk -F'[ ]' '{print $4}')";
 vault login -no-print "$LOGIN_TOKEN";
-check_vault_error "$?" "Login successful." "There was an error loging into Vault.";
+check_vault_error "$?" "Login successful." "There was an error while logging into Vault.";
 LOGIN_TOKEN="";
 
 if [ "${ENABLE_AUDITING,,}" = "true" ] ; then
