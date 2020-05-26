dd7974bb9491e6fe7960aba735606bd445fc975f
Michal Opala <sk4zuzu@gmail.com>
2020-05-26 09:43:31 +0200

    epicli: objdict to/from conversion refactor

--- core/src/epicli/cli/helpers/objdict_helpers.py
+++ core/src/epicli/cli/helpers/objdict_helpers.py
@@ -2,30 +2,29 @@ from cli.helpers.ObjDict import ObjDict
 from copy import deepcopy
 
 
-def nested_dict_to_objdict(d):
-    for k, v in d.items():
-        if isinstance(v, dict):
-            nested_dict_to_objdict(v)
-            d[k] = ObjDict(v)
+def _nested_dict_to_dict(d, *, src_class, dst_class):
+    if isinstance(d, src_class):
+        return dst_class(
+            (k, _nested_dict_to_dict(v, src_class=src_class, dst_class=dst_class))
+            for k, v in d.items()
+        )
+    if isinstance(d, list):
+        return list(
+            _nested_dict_to_dict(v, src_class=src_class, dst_class=dst_class)
+            for v in d
+        )
+    # Clone everything unexpected
+    return deepcopy(d)
 
 
 def dict_to_objdict(d):
-    dc = deepcopy(d)
-    nested_dict_to_objdict(dc)
-    return ObjDict(dc)
-
-
-def nested_objdict_to_dict(d):
-    for k, v in d.items():
-        if isinstance(v, ObjDict):
-            nested_objdict_to_dict(v)
-            d[k] = dict(v)
+    """Recursively convert any dictionary (subclass of dict) to ObjDict."""
+    return _nested_dict_to_dict(d, src_class=dict, dst_class=ObjDict)
 
 
 def objdict_to_dict(d):
-    dc = deepcopy(d)
-    nested_objdict_to_dict(dc)
-    return dict(dc)
+    """Recursively convert any ObjDict to python dictionary."""
+    return _nested_dict_to_dict(d, src_class=dict, dst_class=dict)  # ObjDict is a subclass of dict
 
 
 def merge_objdict(to_merge, extend_by):
@@ -43,7 +42,7 @@ def remove_value(d, value):
     if isinstance(d, str):
         return
     elif isinstance(d, list):
-        for dd in d:     
+        for dd in d:
             remove_value(dd, value)
     else:
         for k in list(d):
@@ -53,4 +52,3 @@ def remove_value(d, value):
             else:
                 if value == v:
                     del d[k]
-        
