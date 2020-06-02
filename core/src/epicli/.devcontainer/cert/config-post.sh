ce1768877b179ee95e64c82de0e920aefb11acb2
Luuk van Venrooij <luukvanvenrooij84@gmail.com>
2020-06-02 18:23:21 +0200

    Custom certs enhancement
    - Added ability to add multiple CA certs and a CA bundle for the devcontainer.

--- core/src/epicli/.devcontainer/cert/config-post.sh
+++ core/src/epicli/.devcontainer/cert/config-post.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 if ls /usr/local/share/ca-certificates/*.crt 1> /dev/null 2>&1; then
-  echo "Setup cert for Ansible, AWS..."
-  pattern="/usr/local/share/ca-certificates/*.crt"
-  files=( $pattern )
-  f="${files[0]}"
   mkdir "/home/vscode/.aws/"
+  if ls /*.pem 1> /dev/null 2>&1; then
+    pattern="/*.pem"
+    files=( $pattern )
+    f="${files[0]}"
+    echo "Setup AWS ca-bundle $f"
+    echo -e "[default]\nca_bundle=$f" >> "/home/vscode/.aws/config"
+  else 
+    pattern="/usr/local/share/ca-certificates/*.crt"
+    files=( $pattern )
+    f="${files[0]}"
+    echo "Setup AWS cert $f"
+    echo -e "[default]\nca_bundle=$f" >> "/home/vscode/.aws/config"
+  fi
+  echo "Setup Ansible SSH timeout settings"
   echo -e "[defaults]\ntimeout=0" >> "/home/vscode/.ansible.cfg"
-  echo -e "[default]\nca_bundle=$f" >> "/home/vscode/.aws/config"
 else
-     echo "No cert to setup"
+  echo "No cert/ca-bundle to setup"
 fi
 
--- core/src/epicli/.devcontainer/cert/config-pre.sh
+++ core/src/epicli/.devcontainer/cert/config-pre.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
 if ls /usr/local/share/ca-certificates/*.crt 1> /dev/null 2>&1; then
-  echo "Setup cert for System, PIP..."
   pattern="/usr/local/share/ca-certificates/*.crt"
   files=( $pattern )
-  f="${files[0]}"
-  echo "Processing cert $f"
-  chmod 644 $f
-  pip config set global.cert $f
-  pip config list
+  for i in "${files[@]}"
+  do
+    chmod 644 $i
+  done
+  if ls /*.pem 1> /dev/null 2>&1; then
+    pattern="/*.pem"
+    files=( $pattern )
+    f="${files[0]}"
+    echo "Setting PIP ca-bundle $f"
+    chmod 644 $f
+    pip config set global.cert $f
+    pip config list
+  else 
+    f="${files[0]}"
+    echo "Setting PIP cert $f"
+    pip config set global.cert $f
+    pip config list
+  fi
   update-ca-certificates
 else
-     echo "No cert to setup"
+  echo "No cert/ca-bundle to setup"
 fi
 
