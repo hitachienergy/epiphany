3ddffe3d5112e4e5ff499476ec206a97b641ed8c
Robert Pudlowski <robert.pudlowski@pl.abb.com>
2020-08-12 13:50:12 +0200

    aks autoscaling feature

--- feature/aks-cluster-creation/main.tf
+++
@@ -1,8 +0,0 @@
-provider "azurerm" {
-  version = "=2.22.0"
-  features {}
-}
-
-terraform {
-	required_version = "=0.12.6"
-}
--- feature/aks-cluster-creation/output.tf
+++
@@ -1,7 +0,0 @@
-output "kube_config" {
-    value = azurerm_kubernetes_cluster.aks.kube_config_raw
-}
-
-output "cluster_endpoint" {
-    value = azurerm_kubernetes_cluster.aks.kube_config.0.host
-}
