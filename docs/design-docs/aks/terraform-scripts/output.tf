d0ca93f28f2f9289696c3f94599a44d03610f5f8
Robert Pudlowski <robert.pudlowski@pl.abb.com>
2020-08-13 17:11:43 +0200

    autoscaling feature

--- docs/design-docs/aks/terraform-scripts/output.tf
+++ docs/design-docs/aks/terraform-scripts/output.tf
@@ -1,5 +1,10 @@
-output "kube_config" {
+output "kubeconfig" {
+    sensitive = true
     value = azurerm_kubernetes_cluster.aks.kube_config_raw
+}
+
+output "kubeconfig_export" {
+    value = "terraform output kubeconfig > ./kubeconfig_${var.prefix} && export KUBECONFIG=./kubeconfig_${var.prefix}"
 }
 
 output "cluster_endpoint" {
