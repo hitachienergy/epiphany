6fb31a2a56ed00170f77fe14c70c48694803fd81
to-bar <46519524+to-bar@users.noreply.github.com>
2020-09-18 21:45:44 +0200

    Improve tunnel verification

--- core/src/epicli/data/common/ansible/playbooks/roles/sshtunnel/files/tunnel.sh
+++ core/src/epicli/data/common/ansible/playbooks/roles/sshtunnel/files/tunnel.sh
@@ -4,9 +4,17 @@ set -o errexit -o nounset -o pipefail
 
 export AUTOSSH_PIDFILE=/tmp/autossh-kubectl-tunnel.pid
 
-if [ "$1" = "--kill" ]; then
-  LOCAL_PORT="$2"
+is_port_available() {
+  local port=$1
 
+  # nc command is much faster than lsof and doesn't require sudo to take into account ports used by other users
+  nc -z 127.0.0.1 "$port" && return 1 || return 0 # 0 if port is available
+}
+
+if [ "$1" = "--kill" ]; then
+  LOCAL_PORT=$2
+
+  # kill autossh (ssh is child process)
   if test -f $AUTOSSH_PIDFILE; then
     echo "PID file found"
     AUTOSSH_PID=$(<$AUTOSSH_PIDFILE)
@@ -15,18 +23,26 @@ if [ "$1" = "--kill" ]; then
     kill "$AUTOSSH_PID" # removes $AUTOSSH_PIDFILE
   fi
 
-  echo "Checking if local port ($LOCAL_PORT) is free"
-  if lsof -i:"$LOCAL_PORT"; then
-    echo "ERROR: Local port ($LOCAL_PORT) still in use"
-    exit 98
-  else
+  # verify
+  echo "Checking if local port ($LOCAL_PORT) is available"
+  if is_port_available "$LOCAL_PORT"; then
     echo "OK"
+  else
+    echo "ERROR: Local port ($LOCAL_PORT) is in use" 1>&2
+    exit 98
   fi
 else # open tunnel
   LOCAL_PORT="$1"
   shift
-  echo "autossh $@"
+  echo "autossh" "$@"
   autossh "$@"
-  # verify (fails if port is not in use)
-  lsof -i:"$LOCAL_PORT"
+  echo "Verifying"
+  # wait for tunnel
+  if timeout 20 sh -c "until nc -z 127.0.0.1 $LOCAL_PORT; do sleep 0.5; done"; then
+    echo "OK"
+  else
+    RC=$?
+    echo "ERROR: Connect timeout" 1>&2
+    exit $RC
+  fi
 fi
