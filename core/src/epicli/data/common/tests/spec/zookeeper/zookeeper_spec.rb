3456bc76fa088c543fa15c12716c79b6385c7a17
to-bar <46519524+to-bar@users.noreply.github.com>
2021-01-14 23:33:04 +0100

    Change AdminServer port from 8080 -> 8008

--- core/src/epicli/data/common/tests/spec/zookeeper/zookeeper_spec.rb
+++ core/src/epicli/data/common/tests/spec/zookeeper/zookeeper_spec.rb
@@ -6,7 +6,8 @@ zookeeper_host = 'localhost'
 zookeeper_client_port = 2181
 zookeeper_peer_port = 2888
 zookeeper_leader_port = 3888
-zookeeper_admin_server_port = 8080
+# TODO: read zookeeper_admin_server_port with: grep -Po '(?<=^admin\.serverPort=)\d+' /opt/zookeeper/conf/zoo.cfg
+zookeeper_admin_server_port = 8008
 
 describe 'Check if ZooKeeper service is running' do
   describe service('zookeeper') do
