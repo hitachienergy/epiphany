3e79e8ab03d7d0d43da5acf0c21975ec2552db35
Michał Opala <sk4zuzu@gmail.com>
2021-01-20 15:50:39 +0100

    tools: add cgroup driver patcher (to enable systemd driver) (#1981)
    * tools: add cgroup driver patcher (to enable systemd driver)
    
    * Apply suggestions from code review
    
    Co-authored-by: to-bar <46519524+to-bar@users.noreply.github.com>
    
    * tools/patch_cgroup_driver: add rollback procedure + use to_nice_json
    
    Co-authored-by: to-bar <46519524+to-bar@users.noreply.github.com>

---
+++ tools/development/k8s/memory/patch_cgroup_driver/apply.sh
@@ -0,0 +1,11 @@
+#/usr/bin/env sh
+
+set -e
+
+read -p "This script will stop and reconfigure kubelet/docker services, then reboot all k8s worker nodes. Are you sure (y/N)? " CHOICE
+case "$CHOICE" in
+  y|yes|Y|Yes) ;;
+  *) exit ;;
+esac
+
+ansible-playbook -vv main.yml
