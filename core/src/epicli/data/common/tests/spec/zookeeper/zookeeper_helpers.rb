2db3c497cfec546fb200a30b440cf2ca970e852d
to-bar <46519524+to-bar@users.noreply.github.com>
2021-02-12 14:05:33 +0100

    [Automated tests] Raise error when remote command fails (#2048)
    * Raise error if remote command fails
    
    * Fail on RuntimeError
    
    * Remove unused TEST_ENV_NUMBER

--- core/src/epicli/data/common/tests/spec/zookeeper/zookeeper_helpers.rb
+++ core/src/epicli/data/common/tests/spec/zookeeper/zookeeper_helpers.rb
@@ -4,6 +4,11 @@ def get_zookeeper_admin_server_port
   grep_cmd = "sudo -u zookeeper grep -Po '(?<=^admin\\.serverPort=)\\d+' #{config_file}"
 
   result = Specinfra.backend.run_command(grep_cmd)
+
+  if (result.failure? and !result.stderr.empty?)
+    raise(result.stderr)
+  end
+
   if /^[0-9]+$/.match?(result.stdout)
     port = result.stdout.chomp.to_i
   else
