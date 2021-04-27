6177b5a6a4b7678d3876ef8c2955a5a766194181
Luuk van Venrooij <11056665+seriva@users.noreply.github.com>
2021-04-27 17:11:56 +0200

    Minor fix for overriding input.

--- core/src/epicli/cli/helpers/config_merger.py
+++ core/src/epicli/cli/helpers/config_merger.py
@@ -1,6 +1,8 @@
+from copy import deepcopy
+
 from cli.helpers.doc_list_helpers import select_first
 from cli.helpers.data_loader import load_all_yaml_objs, types
-from cli.helpers.objdict_helpers import merge_objdict
+from cli.helpers.objdict_helpers import merge_objdict, dict_to_objdict
 from cli.version import VERSION
 
 
@@ -8,9 +10,10 @@ def merge_with_defaults(provider, feature_kind, config_selector, docs):
     files = load_all_yaml_objs(types.DEFAULT, provider, feature_kind)
     config_spec = select_first(files, lambda x: x.name == config_selector)
     if config_selector != 'default':
-        default_config = select_first(docs, lambda x: x.name == 'default' and x.kind == feature_kind)
-        if default_config is None:
-            default_config = select_first(files, lambda x: x.name == 'default')
+        default_config = dict_to_objdict(deepcopy(select_first(docs, lambda x: x.name == 'default' and x.kind == feature_kind)))
+        default_doc = select_first(files, lambda x: x.name == 'default')
+        if default_config is not None:
+            merge_objdict(default_doc, default_config)
         default_config['version'] = VERSION
         merge_objdict(default_config, config_spec)
         return default_config
