efe197ea4eb846d4f2888a30d3593772f88bb56b
Irek Glownia <ireneusz.glownia@hitachi-powergrids.com>
2021-05-12 15:11:28 +0200

    Fix for issue #2318

--- core/src/epicli/data/common/ansible/playbooks/roles/repository/files/download-requirements/redhat-7/download-requirements.sh
+++ core/src/epicli/data/common/ansible/playbooks/roles/repository/files/download-requirements/redhat-7/download-requirements.sh
@@ -615,6 +615,11 @@ else
 	fi
 fi
 
+# Fix for RHUI client certificate expiration [#2318]
+if is_repo_enabled "rhui-microsoft-azure-rhel"; then
+    run_cmd_with_retries yum update -y --disablerepo='*' --enablerepo='rhui-microsoft-azure-rhel*' 3
+fi
+
 # --- Install required packages unless present ---
 
 # repos can be enabled or disabled using the yum-config-manager command, which is provided by yum-utils package
