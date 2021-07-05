e41c11434b89b8dbc833e0fc332096a20fa12b8e
Luuk van Venrooij <11056665+seriva@users.noreply.github.com>
2021-07-05 15:41:50 +0200

    Added #2127

--- core/src/epicli/cli/engine/ApplyEngine.py
+++ core/src/epicli/cli/engine/ApplyEngine.py
@@ -61,7 +61,7 @@ class ApplyEngine(Step):
             raise Exception('No cluster model defined in input YAML file')
 
         # Validate input documents
-        with SchemaValidator(self.cluster_model, self.input_docs) as schema_validator:
+        with SchemaValidator(self.cluster_model.provider, self.input_docs) as schema_validator:
             schema_validator.run()
 
     def process_infrastructure_docs(self):
@@ -74,7 +74,7 @@ class ApplyEngine(Step):
             self.infrastructure_docs = infrastructure_builder.run()
 
         # Validate infrastructure documents
-        with SchemaValidator(self.cluster_model, self.infrastructure_docs) as schema_validator:
+        with SchemaValidator(self.cluster_model.provider, self.infrastructure_docs) as schema_validator:
             schema_validator.run()
 
     def process_configuration_docs(self):
@@ -83,7 +83,7 @@ class ApplyEngine(Step):
             self.configuration_docs = config_appender.run()
 
         # Validate configuration documents
-        with SchemaValidator(self.cluster_model, self.configuration_docs) as schema_validator:
+        with SchemaValidator(self.cluster_model.provider, self.configuration_docs) as schema_validator:
             schema_validator.run()
 
     def collect_infrastructure_config(self):
