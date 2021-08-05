48071048f3b71573e287ee4b7a9c26c06499ed41
atsikham <atsikham@users.noreply.github.com>
2021-08-05 12:30:56 +0200

    Upgrade docker

--- core/src/epicli/data/common/ansible/playbooks/roles/repository/files/download-requirements/centos-7/add-repositories.aarch64.sh
+++ core/src/epicli/data/common/ansible/playbooks/roles/repository/files/download-requirements/centos-7/add-repositories.aarch64.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/env bash -eu
+
+CENTOS_EXTRAS=$(cat <<'EOF'
+[centos-extras]
+name=Centos extras - aarch64
+baseurl=http://mirror.centos.org/altarch/7/extras/aarch64
+enabled=1
+gpgcheck=1
+gpgkey=http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-7
+EOF
+)
+
+add_repo_as_file 'centos-extras' "$CENTOS_EXTRAS" # for Docker dependencies
--- core/src/epicli/data/common/ansible/playbooks/roles/repository/files/download-requirements/centos-7/add-repositories.x86_64.sh
+++ core/src/epicli/data/common/ansible/playbooks/roles/repository/files/download-requirements/centos-7/add-repositories.x86_64.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash -eu
 
+CENTOS_EXTRAS=$(cat <<'EOF'
+[centos-extras]
+name=Centos extras - x86_64
+baseurl=http://mirror.centos.org/centos/7/extras/x86_64
+enabled=1
+gpgcheck=1
+gpgkey=http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-7
+EOF
+)
+
+add_repo_as_file 'centos-extras' "$CENTOS_EXTRAS" # for Docker dependencies
 add_repo_from_script 'https://dl.2ndquadrant.com/default/release/get/10/rpm' # for repmgr
 disable_repo '2ndquadrant-dl-default-release-pg10-debug' # script adds 2 repositories, only 1 is required
