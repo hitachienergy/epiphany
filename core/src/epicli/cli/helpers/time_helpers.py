be97373714a9582fd08ead694999a476ca57f84e
seriva <luukvanvenrooij84@gmail.com>
2021-08-05 15:41:18 +0200

    Minor fixes for time_helpers.py

--- core/src/epicli/cli/helpers/time_helpers.py
+++ core/src/epicli/cli/helpers/time_helpers.py
@@ -1,8 +1,20 @@
-def format_time(time):
-    hours, rem = divmod(time, 3600)
-    minutes, seconds = divmod(rem, 60)
+from math import floor
+
+def format_time(secs):
+    """Formats seconds to a time string with the format: "Xh Xm X.XXs", "Xm X.XXs", "X.XXs"
+
+    Parameters:
+    secs (float): Time in seconds
+
+    Returns:
+    string: Formatted time string
+   """
+
+    seconds = secs%60
+    minutes = floor((secs/60)%60)
+    hours = floor(secs/(60*60)%24)
     if hours != 0:
-        return "%dh %m %.2fs" % (hours, minutes, seconds)
+        return "%dh %dm %.2fs" % (hours, minutes, seconds)
     elif hours == 0 and minutes != 0:
         return "%dm %.2fs" % (minutes, seconds)
     else:
