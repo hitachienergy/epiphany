15c1c4b3a4dabeb6021bfd69ee4b6e07df3646e4
seriva <luukvanvenrooij84@gmail.com>
2021-08-05 12:50:25 +0200

    - Added one more test.

--- core/src/epicli/tests/helpers/test_time_helpers.py
+++ core/src/epicli/tests/helpers/test_time_helpers.py
@@ -6,6 +6,16 @@ def test_format_time_only_seconds():
     assert time_str == "2.22s"
 
 
+def test_format_time_only_minutes():
+    time_str = format_time(120000)
+    assert time_str == "2m 0.00s"
+
+
+def test_format_time_only_hours():
+    time_str = format_time(7200000)
+    assert time_str == "2h 0m 0.00s"
+
+
 def test_format_time_minutes_seconds():
     time_str = format_time(122222)
     assert time_str == "2m 2.22s"
@@ -14,8 +24,3 @@ def test_format_time_minutes_seconds():
 def test_format_time_hours_minutes_seconds():
     time_str = format_time(7322222)
     assert time_str == "2h 2m 2.22s"
-
-
-def test_format_time_only_hours():
-    time_str = format_time(7200000)
-    assert time_str == "2h 0m 0.00s"
