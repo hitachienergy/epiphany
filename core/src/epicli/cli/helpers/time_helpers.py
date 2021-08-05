23f2f67a0dc694b70cd704c8447cdaf69aed90f6
seriva <luukvanvenrooij84@gmail.com>
2021-08-05 13:41:03 +0200

    - MS to S for format_time

--- core/src/epicli/cli/helpers/time_helpers.py
+++ core/src/epicli/cli/helpers/time_helpers.py
@@ -1,9 +1,9 @@
 from math import floor
 
 def format_time(time):
-    seconds = (time/1000)%60
-    minutes = floor((time/(1000*60))%60)
-    hours = floor(time/(1000*60*60)%24)
+    seconds = time%60
+    minutes = floor((time/60)%60)
+    hours = floor(time/(60*60)%24)
     if hours != 0:
         return "%dh %dm %.2fs" % (hours, minutes, seconds)
     elif hours == 0 and minutes != 0:
--- core/src/epicli/tests/helpers/test_time_helpers.py
+++ core/src/epicli/tests/helpers/test_time_helpers.py
@@ -2,25 +2,25 @@ from cli.helpers.time_helpers import format_time
 
 
 def test_format_time_only_seconds():
-    time_str = format_time(2222)
+    time_str = format_time(2.222)
     assert time_str == "2.22s"
 
 
 def test_format_time_only_minutes():
-    time_str = format_time(120000)
+    time_str = format_time(120.000)
     assert time_str == "2m 0.00s"
 
 
 def test_format_time_only_hours():
-    time_str = format_time(7200000)
+    time_str = format_time(7200.000)
     assert time_str == "2h 0m 0.00s"
 
 
 def test_format_time_minutes_seconds():
-    time_str = format_time(122222)
+    time_str = format_time(122.222)
     assert time_str == "2m 2.22s"
 
 
 def test_format_time_hours_minutes_seconds():
-    time_str = format_time(7322222)
+    time_str = format_time(7322.222)
     assert time_str == "2h 2m 2.22s"
