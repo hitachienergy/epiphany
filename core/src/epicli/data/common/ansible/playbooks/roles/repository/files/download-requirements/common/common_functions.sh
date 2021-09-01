hitachienergy/epiphany

commit a72b7024f0a1003fa001b17ed20d1e5c96a2ecd0

Add Internet connection test to download-requirements.sh (2124) (#2493)
* Add diagnostic functions  for internet connection check (curl, wget, apt, yum, crane)

* Add check_connection to download-requirements scripts (ubuntu-18.04, redhat-7, centos-7)

* Move common echol functions to common/common_functions.sh

core/src/epicli/data/common/ansible/playbooks/roles/repository/files/download-requirements/common/common_functions.sh
@@ -1,0 +1,239 @@
+last_error=''  # holds last occurred error msg
+
+
+echol() {
+#
+# Print to stdout, optionally to a log file.
+# Requires $CREATE_LOGFILE and $LOG_FILE_PATH to be defined.
+#
+# :param $@: args to be printed
+#
+    echo -e "$@"
+    if [[ $CREATE_LOGFILE == "yes" ]]; then
+        local timestamp=$(date +"%b %e %H:%M:%S")
+        echo -e "${timestamp}: $@" >> "$LOG_FILE_PATH"
+    fi
+}
+
+
+exit_with_error() {
+    echol $@
+    exit 1
+}
+
+
+__at_least_one_test_pass() {
+#
+# Iterate over all arguments each time call test $function and check result.
+# If at least one call passes, function will yield success.
+#
+# :param $1: test function
+# :param $@: arguments to be tested
+# :return: 0 - success, 1 - failure
+    local function=$1
+    shift
+
+    local args=$@
+    local total_count=$#
+    local failed_count=0
+
+    for arg in $args; do
+        echol "- $arg..."
+        $function $arg
+
+        (( $? == 0 )) && return 0
+
+        failed_count=$(( $failed_count + 1 ))
+    done
+
+    (( $total_count != $failed_count )) || return 1
+    return 0
+}
+
+
+__test_address_curl() {
+#
+# Test address connection without downloading any resource.
+#
+# :param $1: url to be tested
+# :return: curl exit value
+#
+    last_error=$(curl --show-error --silent $1 2>&1 >/dev/null)
+    return $?
+}
+
+
+__check_curl() {
+#
+# Use curl in silent mode to check if target `url` is available.
+#
+# :param $@: urls to be tested
+# :return: 0 - success, 1 - failure
+#
+    echol "Testing curl connection:"
+
+    (( $# > 0 )) || exit_with_error "_check_curl: no url provided"
+
+    __at_least_one_test_pass __test_address_curl $@
+}
+
+
+__test_address_wget() {
+#
+# Test address connection without downloading any resource.
+#
+# :param $1: url to be tested
+# :return: wget exit value
+#
+    last_error=$(wget --spider $1 2>&1 >/dev/null)
+    return $?
+}
+
+
+__check_wget() {
+#
+# Use wget in spider mode (without downloading resources) to check if target `url`
+# is available.
+#
+# :param $@: urls to be tested
+# :return: 0 - success, 1 - failure
+#
+    echol "Testing wget connection:"
+
+    (( $# > 0 )) || exit_with_error "_check_wget: no url provided"
+
+    __at_least_one_test_pass __test_address_wget $@
+}
+
+
+__test_apt_repo() {
+#
+# Update a single repository.
+#
+# :param $1: repository to be updated
+# :return: apt return value
+#
+    last_error=$(apt update -o Dir::Etc::sourcelist=$1 2>&1 >/dev/null)
+    return $?
+}
+
+
+__check_apt() {
+#
+# Use `apt update` to make sure that there is connection to repositories.
+#
+# :param $@: repos to be tested
+# :return: 0 - success, 1 - failure
+#
+    echol "Testing apt connection:"
+
+    (( $# > 0 )) || exit_with_error "_check_apt: no repositories provided"
+    local repos=$@
+
+    (( $UID == 0 )) || exit_with_error "apt needs to be run as a root"
+
+    __at_least_one_test_pass __test_apt_repo $repos
+    return $?
+}
+
+
+__test_yum_repo() {
+#
+# List packages from a single repository.
+#
+# :param $1: repository to be listed
+# :return: yum return value
+#
+    last_error=$(yum --quiet --disablerepo=* --enablerepo=$1 list available 2>&1 >/dev/null)
+    return $?
+}
+
+
+__check_yum() {
+#
+# Use `yum list` to make sure that there is connection to repositories.
+# Query available packages for each repository.
+#
+# :param $@: repositories to be tested
+# :return: 0 - success, 1 - failure
+#
+    echol "Testing yum connection:"
+
+    (( $# > 0 )) || exit_with_error "_check_yum: no repositories provided"
+    local repos=$@
+
+    __at_least_one_test_pass __test_yum_repo $repos
+    return $?
+}
+
+
+__test_crane_repo() {
+#
+# List packages from a single repository.
+# Requires $CRANE_BIN to be defined
+#
+# :param $1: repository to be listed
+# :return: crane return value
+#
+    last_error=$($CRANE_BIN ls $1 2>&1 >/dev/null)
+    return $?
+}
+
+
+__check_crane() {
+#
+# Use `crane ls` to make sure that there is connection to repositories.
+# Query available packages for each repository.
+#
+# :param $@: repositories to be tested
+# :return: 0 - success, 1 - failure
+#
+    echol "Testing crane connection:"
+
+    (( $# > 0 )) || exit_with_error "_check_crane: no repository provided"
+    local repos=$@
+
+    __at_least_one_test_pass __test_crane_repo $repos
+    return $?
+}
+
+
+# Tools which can be tested:
+declare -A tools=(
+[curl]=_check_curl
+[wget]=_check_wget
+[apt]=_check_apt
+[yum]=_check_yum
+[crane]=_check_crane
+)
+
+
+check_connection() {
+#
+# Run connection test for target `tool` with optional `url` parameter.
+# Requires $internet_access_checks_enabled to be defined.
+#
+# :param $1: which `tool` to test
+# :param $@: optional parameters used by some tools such as `url`
+#
+    [[ $internet_access_checks_enabled == "no" ]] && return 0
+
+    [[ $# -lt 1 ]] && exit_with_error '"tool" argument not provided'
+    local tool=$1
+
+    shift  # discard tool variable
+
+    [[ ! -n ${tools[$tool]} ]] && exit_with_error "no such tool: \"$tool\""
+
+    (  # disable -e in order to handle non-zero return values
+        set +e
+
+        ${tools[$tool]} $@
+
+        if (( $? == 0 )); then
+            echol "Connection successful."
+        else
+            exit_with_error "Connection failure, reason: [$last_error]"
+        fi
+    )
+}

core/src/epicli/cli/engine/PrepareEngine.py
@@ -11,8 +11,9 @@ from cli.helpers.build_saver import copy_files_recursively
 
 
 class PrepareEngine(Step):
-    PREPARE_PATH = DATA_FOLDER_PATH + '/common/ansible/playbooks/roles/repository/files/download-requirements'
-    CHARTS_PATH = DATA_FOLDER_PATH + '/common/ansible/playbooks/roles/helm_charts/files/system'
+    PREPARE_PATH = f'{DATA_FOLDER_PATH}/common/ansible/playbooks/roles/repository/files/download-requirements'
+    COMMON_PATH = f'{PREPARE_PATH}/common'
+    CHARTS_PATH = f'{DATA_FOLDER_PATH}/common/ansible/playbooks/roles/helm_charts/files/system'
 
     def __init__(self, input_data):
         super().__init__(__name__)
@@ -27,7 +28,6 @@ class PrepareEngine(Step):
 
     def prepare(self):
         prepare_src = os.path.join(self.PREPARE_PATH, self.os)
-        charts_src = self.CHARTS_PATH
 
         prepare_dst = os.path.join(Config().output_dir, 'prepare_scripts')
         charts_dst = os.path.join(prepare_dst, 'charts', 'system')
@@ -38,7 +38,8 @@ class PrepareEngine(Step):
 
         # copy files to output dir
         copy_files_recursively(prepare_src, prepare_dst)
-        copy_files_recursively(charts_src, charts_dst)
+        copy_files_recursively(self.COMMON_PATH, os.path.join(prepare_dst, 'common'))
+        copy_files_recursively(self.CHARTS_PATH, charts_dst)
 
         # make sure the scripts are executable
         self.make_file_executable(os.path.join(prepare_dst, 'download-requirements.sh'))

core/src/epicli/data/common/ansible/playbooks/roles/repository/files/download-requirements/centos-7/download-requirements.sh
@@ -7,6 +7,11 @@
 
 set -euo pipefail
 
+# set variables needed by common_functions
+readonly internet_access_checks_enabled="yes"
+readonly script_path="$(readlink -f $(dirname $0))"
+. "${script_path}/common/common_functions.sh"
+
 # === Functions (in alphabetical order) ===
 
 # params: <repo_id> <repo_url>
@@ -149,14 +154,6 @@ download_packages() {
 	fi
 }
 
-echol() {
-	echo -e "$@"
-	if [[ $CREATE_LOGFILE == 'yes' ]]; then
-		local timestamp=$(date +"%b %e %H:%M:%S")
-		echo -e "${timestamp}: $@" >> "$LOG_FILE_PATH"
-	fi
-}
-
 # params: <repo_id>
 enable_repo() {
 	local repo_id="$1"
@@ -166,11 +163,6 @@ enable_repo() {
 		yum-config-manager --enable "$repo_id" ||
 			exit_with_error "Command failed: yum-config-manager --enable \"$repo_id\""
 	fi
-}
-
-exit_with_error() {
-	echol "ERROR: $1"
-	exit 1
 }
 
 # params: <result_var> <package>
@@ -597,6 +589,8 @@ get_requirements_from_group 'IMAGES'               'images'                "$REQ
 
 # === Packages ===
 
+check_connection yum $(yum repolist --quiet | tail -n +2 | cut -d' ' -f1 | cut -d'/' -f1 | sed 's/^!//')
+
 # --- Backup yum repositories ---
 
 if [ -f "$YUM_CONFIG_BACKUP_FILE_PATH" ]; then
@@ -631,6 +625,9 @@ else
         echol "Crane binary already exists"
 	else
 		file_url=$(head -n 1 <<< "${CRANE}")
+
+		check_connection wget $file_url
+
 		echol "Downloading crane from: ${file_url}"
 		download_file "${file_url}" "${SCRIPT_DIR}"
 		tar_path="${SCRIPT_DIR}/${file_url##*/}"
@@ -739,6 +736,8 @@ fi
 
 # === Files ===
 
+check_connection wget $FILES
+
 create_directory "$FILES_DIR"
 
 if [[ -z "$FILES" ]]; then
@@ -763,6 +762,8 @@ else
 fi
 
 # === Images ===
+
+check_connection crane $(for image in $IMAGES; do splitted=(${image//:/ }); echo "${splitted[0]}"; done)
 
 create_directory "$IMAGES_DIR"
 

core/src/epicli/data/common/ansible/playbooks/roles/repository/files/download-requirements/redhat-7/download-requirements.sh
@@ -7,6 +7,11 @@
 
 set -euo pipefail
 
+# set variables needed by common_functions
+readonly internet_access_checks_enabled="yes"
+readonly script_path="$(readlink -f $(dirname $0))"
+. "${script_path}/common/common_functions.sh"
+
 # === Functions (in alphabetical order) ===
 
 # params: <repo_id> <repo_url>
@@ -149,14 +154,6 @@ download_packages() {
 	fi
 }
 
-echol() {
-	echo -e "$@"
-	if [[ $CREATE_LOGFILE == 'yes' ]]; then
-		local timestamp=$(date +"%b %e %H:%M:%S")
-		echo -e "${timestamp}: $@" >> "$LOG_FILE_PATH"
-	fi
-}
-
 # params: <repo_id>
 enable_repo() {
 	local repo_id="$1"
@@ -166,11 +163,6 @@ enable_repo() {
 		yum-config-manager --enable "$repo_id" ||
 			exit_with_error "Command failed: yum-config-manager --enable \"$repo_id\""
 	fi
-}
-
-exit_with_error() {
-	echol "ERROR: $1"
-	exit 1
 }
 
 # desc: find repo id (set $1) based on given pattern
@@ -616,6 +608,8 @@ get_requirements_from_group 'IMAGES'               'images'                "$REQ
 
 # --- Backup yum repositories ---
 
+check_connection yum $(yum repolist --quiet | tail -n +2 | cut -d' ' -f1 | cut -d'/' -f1 | sed 's/^!//')
+
 if [ -f "$YUM_CONFIG_BACKUP_FILE_PATH" ]; then
 	echol "Backup aleady exists: $YUM_CONFIG_BACKUP_FILE_PATH"
 else
@@ -653,6 +647,9 @@ else
         echol "Crane binary already exists"
 	else
 		file_url=$(head -n 1 <<< "${CRANE}")
+
+		check_connection wget $file_url
+
 		echol "Downloading crane from: ${file_url}"
 		download_file "${file_url}" "${SCRIPT_DIR}"
 		tar_path="${SCRIPT_DIR}/${file_url##*/}"
@@ -768,9 +765,12 @@ fi
 
 create_directory "$FILES_DIR"
 
+check_connection wget $FILES
+
 if [[ -z "$FILES" ]]; then
     echol "No files to download"
 else
+
     # list of all files that will be downloaded
     echol "Files to be downloaded:"
     cat -n <<< "${FILES}"
@@ -790,6 +790,8 @@ else
 fi
 
 # === Images ===
+
+check_connection crane $(for image in $IMAGES; do splitted=(${image//:/ }); echo "${splitted[0]}"; done)
 
 create_directory "$IMAGES_DIR"
 

core/src/epicli/data/common/ansible/playbooks/roles/repository/files/download-requirements/ubuntu-18.04/add-repositories.sh
@@ -31,5 +31,3 @@ echo "deb http://apt-archive.postgresql.org/pub/repos/apt bionic-pgdg-archive ma
 # Provides repmgr
 wget -qO - https://dl.2ndquadrant.com/gpg-key.asc | apt-key add -
 echo "deb https://dl.2ndquadrant.com/default/release/apt bionic-2ndquadrant main" | tee -a /etc/apt/sources.list.d/2ndquadrant-dl-default-release.list
-
-apt update

core/src/epicli/data/common/ansible/playbooks/roles/repository/files/download-requirements/ubuntu-18.04/common.sh
@@ -8,15 +8,6 @@ usage() {
 	exit 1
 }
 
-echol() {
-	echo -e "$1" | tee --append "$logfile"
-}
-
-exit_with_error() {
-	echol "ERROR: $1"
-	exit 1
-}
-
 # params: <file_path>
 remove_file() {
 	local file_path="$1"
@@ -51,9 +42,9 @@ download_image() {
 	else
 		local tmp_file=$(mktemp)
 		echo "Downloading image: $1"
-		echo "Crane command is: ${crane_bin} pull --insecure --format=legacy ${image_name} ${dst_image}"
+		echo "Crane command is: ${CRANE_BIN} pull --insecure --format=legacy ${image_name} ${dst_image}"
 		# use temporary file for downloading to be safe from sudden interruptions (network, ctrl+c)
-		run_cmd_with_retries $retries ${crane_bin} pull --insecure --platform=${docker_platform} --format=legacy ${image_name} ${tmp_file} && chmod 644 ${tmp_file} && mv ${tmp_file} ${dst_image}
+		run_cmd_with_retries $retries ${CRANE_BIN} pull --insecure --platform=${docker_platform} --format=legacy ${image_name} ${tmp_file} && chmod 644 ${tmp_file} && mv ${tmp_file} ${dst_image}
 	fi
 }
 

core/src/epicli/data/common/ansible/playbooks/roles/repository/files/download-requirements/ubuntu-18.04/download-requirements.sh
@@ -8,6 +8,12 @@ script_path="$( cd "$(dirname "$0")" ; pwd -P )"
 # source common functions
 . "${script_path}/common.sh"
 
+internet_access_checks_enabled="yes"
+CREATE_LOGFILE="yes"
+LOG_FILE_PATH="${script_path}/log"
+
+. "${script_path}/common/common_functions.sh"
+
 if [[ $# -lt 1 ]]; then
 	usage
 fi
@@ -20,11 +26,10 @@ dst_dir_packages="${dst_dir}/packages"
 dst_dir_files="${dst_dir}/files"
 dst_dir_images="${dst_dir}/images"
 deplist="${script_path}/.dependencies"
-logfile="${script_path}/log"
 retries="3"
 download_cmd="run_cmd_with_retries $retries apt-get download"
 add_repos="${script_path}/add-repositories.sh"
-crane_bin="${script_path}/crane"
+CRANE_BIN="${script_path}/crane"
 
 # arch
 arch=$(uname -m)
@@ -85,6 +90,9 @@ shopt -u nullglob
 # TODO: See if we need to split this up to support different architectures
 . "${add_repos}"
 
+check_connection apt $(ls /etc/apt/sources.list.d)
+apt update
+
 # parse the input file, separete by tags: [crane], [packages], [files], [images]
 crane=$(awk '/^$/ || /^#/ {next}; /\[crane\]/ {f=1; next}; /^\[/ {f=0}; f {print $0}' "${input_file}")
 packages=$(awk '/^$/ || /^#/ {next}; /\[packages\]/ {f=1; next}; /^\[/ {f=0}; f {print $0}' "${input_file}")
@@ -104,23 +112,28 @@ fi
 if [[ -z "${crane}" || $(wc -l <<< "${crane}") -ne 1 ]] ; then
     exit_with_error "Crane binary download path undefined or more than one download path defined"
 else
-    if [[ -x $crane_bin ]]; then
+    if [[ -x $CRANE_BIN ]]; then
         echol "Crane binary already exists"
     else
         file_url=$(head -n 1 <<< "${crane}")
+
+        check_connection wget $file_url
+
         echol "Downloading crane from: $file_url"
         download_file "$file_url" "$script_path"
         tar_path="${script_path}/${file_url##*/}"
-        echol "Unpacking crane from $tar_path to $crane_bin"
+        echol "Unpacking crane from $tar_path to $CRANE_BIN"
         tar -xzf "$tar_path" --directory "$script_path" "crane" --overwrite
-        chmod +x "$crane_bin"
+        chmod +x "$CRANE_BIN"
         remove_file "$tar_path"
-        [[ -f $crane_bin ]] || exit_with_error "File not found: $crane_bin"
-        [[ -x $crane_bin ]] || exit_with_error "$crane_bin has to be executable"
+        [[ -f $CRANE_BIN ]] || exit_with_error "File not found: $CRANE_BIN"
+        [[ -x $CRANE_BIN ]] || exit_with_error "$CRANE_BIN has to be executable"
     fi
 fi
 
 printf "\n"
+
+check_connection crane $(for image in $images; do splitted=(${image//:/ }); echo "${splitted[0]}"; done)
 
 # PACKAGES
 # if dependency list doesn't exist or is zero size then resolve dependency and store them in a deplist file
@@ -151,11 +164,13 @@ else
     cat -n "$deplist"
 
     # download dependencies (apt-get sandboxing warning when running as root are harmless)
-    cd "$dst_dir_packages" && xargs --no-run-if-empty --arg-file="$deplist" --delimiter='\n' -I{} bash -c ". ${script_path}/common.sh && ${download_cmd} {}" | tee -a "${logfile}"
+    cd "$dst_dir_packages" && xargs --no-run-if-empty --arg-file="$deplist" --delimiter='\n' -I{} bash -c ". ${script_path}/common.sh && ${download_cmd} {}" | tee -a "${LOG_FILE_PATH}"
     cd "$script_path"
 fi
 
 printf "\n"
+
+check_connection wget $(for file in $files; do echo "$file"; done)
 
 # FILES
 # process files

core/src/epicli/data/common/ansible/playbooks/roles/repository/tasks/setup.yml
@@ -33,6 +33,11 @@
         dest: /tmp/epi-download-requirements/download-requirements.sh
         mode: a+x
 
+    - name: Copy common files for downloading requirements to repository host
+      copy:
+        src: download-requirements/common
+        dest: /tmp/epi-download-requirements
+
   when:
     - not offline_mode
     - not custom_repository_url

commit 2959fadd43b1ac29bc2592ff1c0d5f1c77d092cb

Internet connection check script fix
* Fix bug introduced in a72b7024

core/src/epicli/data/common/ansible/playbooks/roles/repository/files/download-requirements/common/common_functions.sh
@@ -200,11 +200,11 @@ __check_crane() {
 
 # Tools which can be tested:
 declare -A tools=(
-[curl]=_check_curl
-[wget]=_check_wget
-[apt]=_check_apt
-[yum]=_check_yum
-[crane]=_check_crane
+[curl]=__check_curl
+[wget]=__check_wget
+[apt]=__check_apt
+[yum]=__check_yum
+[crane]=__check_crane
 )
 
 