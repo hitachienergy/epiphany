32e83176aa18cd5ce61db973ffd08026c4fbb213
erzetpe <erzetpe@gmail.com>
2021-09-08 11:53:23 +0200

    Further tests and test refactor

--- core/src/epicli/tests/helpers/test_build_saver.py
+++ core/src/epicli/tests/helpers/test_build_saver.py
@@ -1,30 +1,46 @@
+import os
 from cli.helpers.build_saver import get_build_path, get_output_path, get_terraform_path, get_ansible_path,\
-    get_ansible_vault_path, get_ansible_config_file_path
+    get_ansible_vault_path, get_ansible_config_file_path, get_inventory_path,\
+    TERRAFORM_OUTPUT_DIR, ANSIBLE_OUTPUT_DIR, ANSIBLE_VAULT_OUTPUT_DIR, INVENTORY_FILE_NAME
 
 CLUSTER_NAME = "test"
 OUTPUT_PATH = "/workspaces/epiphany/core/src/epicli/test_results/"
-TERRAFORM_OUTPUT_DIR = 'terraform/'
 MANIFEST_FILE_NAME = 'manifest.yml'
 SP_FILE_NAME = 'sp.yml'
-INVENTORY_FILE_NAME = 'inventory'
-ANSIBLE_OUTPUT_DIR = 'ansible/'
-ANSIBLE_VAULT_OUTPUT_DIR = 'vault/'
 SPEC_OUTPUT_DIR = 'spec_tests/'
 
+# TODO: Check directory creation for tests
+# TODO: Sort imports
+
 def test_get_output_path():
-    assert get_output_path() == f"{OUTPUT_PATH}"
+    assert get_output_path() == os.path.join(OUTPUT_PATH)
+
 
 def test_get_build_path():
-    assert get_build_path(CLUSTER_NAME) == f"{OUTPUT_PATH}test"
+    assert get_build_path(CLUSTER_NAME) == os.path.join(
+        OUTPUT_PATH, CLUSTER_NAME)
+
+
+def test_get_inventory_path():
+    assert get_inventory_path(CLUSTER_NAME) == os.path.join(
+        OUTPUT_PATH, CLUSTER_NAME, INVENTORY_FILE_NAME)
+
 
 def test_get_terraform_path():
-    assert get_terraform_path(CLUSTER_NAME) == f"{OUTPUT_PATH}test/terraform/"
+    assert get_terraform_path(CLUSTER_NAME) == os.path.join(
+        OUTPUT_PATH, CLUSTER_NAME, TERRAFORM_OUTPUT_DIR)
+
 
 def test_get_ansible_path():
-    assert get_ansible_path(CLUSTER_NAME) == f"{OUTPUT_PATH}test/ansible/"
+    assert get_ansible_path(CLUSTER_NAME) == os.path.join(
+        OUTPUT_PATH, CLUSTER_NAME, ANSIBLE_OUTPUT_DIR)
+
 
 def test_get_ansible_vault_path():
-    assert get_ansible_vault_path(CLUSTER_NAME) == f"{OUTPUT_PATH}test/vault/"
+    assert get_ansible_vault_path(CLUSTER_NAME) == os.path.join(
+        OUTPUT_PATH, CLUSTER_NAME, ANSIBLE_VAULT_OUTPUT_DIR)
+
 
 def test_get_ansible_config_file_path():
-    assert get_ansible_config_file_path(CLUSTER_NAME) == f"{OUTPUT_PATH}test/ansible/ansible.cfg"
+    assert get_ansible_config_file_path(CLUSTER_NAME) == os.path.join(
+        OUTPUT_PATH, CLUSTER_NAME, ANSIBLE_OUTPUT_DIR, 'ansible.cfg')
