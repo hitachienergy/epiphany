215c98dc36af69afd877b44c79fbcce8d358f121
Sebastian Białobrzecki <sbb@disroot.org>
2021-11-08 15:28:29 +0100

    use pytest instead of unittest

--- tests/unit/engine/providers/any/test_APIProxy.py
+++ tests/unit/engine/providers/any/test_APIProxy.py
@@ -1,33 +1,26 @@
-from unittest import mock, TestCase
+from pytest_mock import MockerFixture
 
 from cli.engine.providers.any.APIProxy import APIProxy
 from cli.models.AnsibleHostModel import AnsibleOrderedHostModel
 from tests.unit.engine.providers.data.APIProxy_data import CLUSTER_MODEL, CONFIG_DOC
 
 
-class APIProxyTest(TestCase):
-    """ Tests for `any` provider """
+def test_get_ips_for_feature(mocker: MockerFixture):
+    """
+    Make sure that hostnames in inventory are sorted.
+    """
 
-    def setUp(self):
-        self.maxDiff = None
+    mocker.patch('cli.engine.providers.any.APIProxy.Log')
+    proxy = APIProxy(CLUSTER_MODEL('any'), CONFIG_DOC())
 
+    EXPECTED_RESULT = [
+        AnsibleOrderedHostModel('service-vm-0', '20.73.105.240'),
+        AnsibleOrderedHostModel('service-vm-1', '20.73.105.188'),
+        AnsibleOrderedHostModel('service-vm-2', '20.73.105.18'),
+        AnsibleOrderedHostModel('service-vm-3', '20.73.105.33'),
+        AnsibleOrderedHostModel('service-vm-4', '20.73.105.54')
+    ]
 
-    def test_get_ips_for_feature(self):
-        """
-        Make sure that hostnames in inventory are sorted.
-        """
+    result = proxy.get_ips_for_feature('service')
 
-        with mock.patch('cli.engine.providers.any.APIProxy.Log', return_value='mock_Log') as mock_Log:
-            proxy = APIProxy(CLUSTER_MODEL('any'), CONFIG_DOC())
-
-            EXPECTED_RESULT = [
-                AnsibleOrderedHostModel('service-vm-0', '20.73.105.240'),
-                AnsibleOrderedHostModel('service-vm-1', '20.73.105.188'),
-                AnsibleOrderedHostModel('service-vm-2', '20.73.105.18'),
-                AnsibleOrderedHostModel('service-vm-3', '20.73.105.33'),
-                AnsibleOrderedHostModel('service-vm-4', '20.73.105.54')
-            ]
-
-            result = proxy.get_ips_for_feature('service')
-
-            self.assertListEqual(EXPECTED_RESULT, result)
+    assert EXPECTED_RESULT == result
--- tests/unit/engine/providers/azure/test_APIProxy.py
+++ tests/unit/engine/providers/azure/test_APIProxy.py
@@ -1,33 +1,27 @@
-from unittest import mock, TestCase
+from pytest_mock import MockerFixture
 
 from cli.engine.providers.azure.APIProxy import APIProxy
 from cli.models.AnsibleHostModel import AnsibleOrderedHostModel
 from tests.unit.engine.providers.data.APIProxy_data import CLUSTER_MODEL, RUNNING_INSTANCES_AZURE
 
 
-class APIProxyTest(TestCase):
-    """ Tests for `azure` provider """
+def test_get_ips_for_feature(mocker: MockerFixture):
+    """
+    Make sure that hostnames in inventory are sorted.
+    """
 
-    def setUp(self):
-        self.maxDiff = None
+    mocker.patch('cli.engine.providers.azure.APIProxy.Log')
+    proxy = APIProxy(CLUSTER_MODEL('azure'), RUNNING_INSTANCES_AZURE)
+    proxy.run = (lambda *args: RUNNING_INSTANCES_AZURE)  # mock run with prepared data
 
-    def test_get_ips_for_feature(self):
-        """
-        Make sure that hostnames in inventory are sorted.
-        """
+    EXPECTED_RESULT = [
+        AnsibleOrderedHostModel('prefix-cluster-service-vm-0', '20.73.105.240'),
+        AnsibleOrderedHostModel('prefix-cluster-service-vm-1', '20.73.105.188'),
+        AnsibleOrderedHostModel('prefix-cluster-service-vm-2', '20.73.105.18'),
+        AnsibleOrderedHostModel('prefix-cluster-service-vm-3', '20.73.105.33'),
+        AnsibleOrderedHostModel('prefix-cluster-service-vm-4', '20.73.105.54')
+    ]
 
-        with mock.patch('cli.engine.providers.azure.APIProxy.Log', return_value='mock_Log') as mock_Log:
-            proxy = APIProxy(CLUSTER_MODEL('azure'), RUNNING_INSTANCES_AZURE)
-            proxy.run = (lambda *args: RUNNING_INSTANCES_AZURE)  # mock run with prepared data
+    result = proxy.get_ips_for_feature('service')
 
-            EXPECTED_RESULT = [
-                AnsibleOrderedHostModel('prefix-cluster-service-vm-0', '20.73.105.240'),
-                AnsibleOrderedHostModel('prefix-cluster-service-vm-1', '20.73.105.188'),
-                AnsibleOrderedHostModel('prefix-cluster-service-vm-2', '20.73.105.18'),
-                AnsibleOrderedHostModel('prefix-cluster-service-vm-3', '20.73.105.33'),
-                AnsibleOrderedHostModel('prefix-cluster-service-vm-4', '20.73.105.54')
-            ]
-
-            result = proxy.get_ips_for_feature('service')
-
-            self.assertListEqual(EXPECTED_RESULT, result)
+    assert EXPECTED_RESULT == result
