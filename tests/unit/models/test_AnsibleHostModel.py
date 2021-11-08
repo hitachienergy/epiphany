bb1195d95dbc6ff15a1924b491df29d21d8aca38
Sebastian Białobrzecki <sbb@disroot.org>
2021-11-08 15:46:16 +0100

    remove unittest from test_AnsibleHostModel.py

--- tests/unit/models/test_AnsibleHostModel.py
+++ tests/unit/models/test_AnsibleHostModel.py
@@ -1,35 +1,29 @@
 from typing import List
-from unittest import TestCase
 
 from cli.models.AnsibleHostModel import AnsibleOrderedHostModel
 
 
-class AnsibleHostModelTest(TestCase):
+def test_sort():
+    """
+    Test the `less` operator
+    """
 
-    def setUp(self):
-        self.maxDiff = None
+    EXPECTED_HOSTS: List[AnsibleOrderedHostModel] = [
+        AnsibleOrderedHostModel('prefix-cluster-service-vm-0', '20.82.14.10'),
+        AnsibleOrderedHostModel('prefix-cluster-service-vm-1', '20.82.14.34'),
+        AnsibleOrderedHostModel('prefix-cluster-service-vm-2', '20.82.14.101'),
+        AnsibleOrderedHostModel('prefix-cluster-service-vm-3', '20.82.14.67'),
+        AnsibleOrderedHostModel('prefix-cluster-service-vm-4', '20.82.14.11'),
+    ]
 
-    def test_sort(self):
-        """
-        Test the `less` operator
-        """
+    unordered_hosts: List[AnsibleOrderedHostModel] = [
+        AnsibleOrderedHostModel('prefix-cluster-service-vm-4', '20.82.14.11'),
+        AnsibleOrderedHostModel('prefix-cluster-service-vm-1', '20.82.14.34'),
+        AnsibleOrderedHostModel('prefix-cluster-service-vm-3', '20.82.14.67'),
+        AnsibleOrderedHostModel('prefix-cluster-service-vm-0', '20.82.14.10'),
+        AnsibleOrderedHostModel('prefix-cluster-service-vm-2', '20.82.14.101')
+    ]
 
-        EXPECTED_HOSTS: List[AnsibleOrderedHostModel] = [
-            AnsibleOrderedHostModel('prefix-cluster-service-vm-0', '20.82.14.10'),
-            AnsibleOrderedHostModel('prefix-cluster-service-vm-1', '20.82.14.34'),
-            AnsibleOrderedHostModel('prefix-cluster-service-vm-2', '20.82.14.101'),
-            AnsibleOrderedHostModel('prefix-cluster-service-vm-3', '20.82.14.67'),
-            AnsibleOrderedHostModel('prefix-cluster-service-vm-4', '20.82.14.11'),
-        ]
+    unordered_hosts.sort()
 
-        unordered_hosts: List[AnsibleOrderedHostModel] = [
-            AnsibleOrderedHostModel('prefix-cluster-service-vm-4', '20.82.14.11'),
-            AnsibleOrderedHostModel('prefix-cluster-service-vm-1', '20.82.14.34'),
-            AnsibleOrderedHostModel('prefix-cluster-service-vm-3', '20.82.14.67'),
-            AnsibleOrderedHostModel('prefix-cluster-service-vm-0', '20.82.14.10'),
-            AnsibleOrderedHostModel('prefix-cluster-service-vm-2', '20.82.14.101')
-        ]
-
-        unordered_hosts.sort()
-
-        self.assertListEqual(EXPECTED_HOSTS, unordered_hosts)
+    self.assertListEqual(EXPECTED_HOSTS, unordered_hosts)
