6c2942d2dc79dd33e21ecc087e612edec366ecd7
to-bar <46519524+to-bar@users.noreply.github.com>
2021-12-30 19:32:05 +0000

    Do not require dpkg-dev

--- ansible/playbooks/roles/repository/files/server/Debian/create-repository.sh
+++ ansible/playbooks/roles/repository/files/server/Debian/create-repository.sh
@@ -2,61 +2,65 @@
 
 epi_repo_server_path=$1 # /var/www/html/epirepo is the default
 is_offline_mode=$2 # true/false
-script_path=$(cd "$(dirname "$0")"; pwd -P)
 apt_repo_path="/etc/apt/sources.list"
+
+apt_update() {
+  echo "Updating list of available packages..."
+  apt -y update
+}
+
+# params: <repo_path>
+generate_repo_metadata() {
+  local repo_path="$1"
+
+  echo "Generating repository metadata..."
+  # 'cd' is needed here because 'dpkg-scanpackages' prepends path to "Filename" field in Packages.gz
+  # otherwise it would break package URL for apt
+  # -m is important because it allows same packages with different versions
+  cd "${repo_path}/packages" \
+  && /var/tmp/epi-repository-setup-scripts/dpkg-scanpackages -m . | gzip -9c > Packages.gz
+}
+
+install_apache() {
+  echo "Installing apache2 package..."
+  # force non-interactive mode, ref: https://bugs.launchpad.net/ubuntu/+source/ansible/+bug/1833013
+  DEBIAN_FRONTEND=noninteractive \
+  UCF_FORCE_CONFOLD=1 \
+    apt-get \
+    -o Dpkg::Options::=--force-confold \
+    -o Dpkg::Options::=--force-confdef \
+    -y -q install apache2
+}
 
 
 if [[ "$is_offline_mode" == "true" ]]; then
-  # bootstrap apache and dpkg-dev installation in air-gap mode
+  # bootstrap apache installation in air-gap mode
   if [[ -f $apt_repo_path ]]; then
       echo "Disabling default repositories..."
       mv $apt_repo_path ${apt_repo_path}.bak
   fi
 
-  if ! dpkg -l | grep -q "^ii  libdpkg-perl\s"; then 
+  if ! dpkg -l | grep -q "^ii  libdpkg-perl\s"; then
     echo "Package libdpkg-perl not found, installing..."
     dpkg -i "${epi_repo_server_path}"/packages/libdpkg-perl*.deb
     retval=$?
     if [[ $retval -ne "0" ]]; then
-      echo "Exiting.."
+      echo "Exiting..."
       exit "$retval"
     fi
   fi
 
-  echo "Generating repository metadata..."
-  cd "${epi_repo_server_path}/packages" && /var/tmp/epi-repository-setup-scripts/dpkg-scanpackages -m . | gzip -9c > Packages.gz && cd "${script_path}"
+  generate_repo_metadata "$epi_repo_server_path"
   echo "deb [trusted=yes] file:${epi_repo_server_path}/packages ./" > /etc/apt/sources.list.d/epilocal.list
-  #apt update --assume-no # workaround for botched docker repository https://github.com/docker/for-linux/issues/812
-  echo "Updating list of available packages..."
-  apt -y update
-  echo "Installing apache..."
-  # force non-interactive mode, ref: https://bugs.launchpad.net/ubuntu/+source/ansible/+bug/1833013
-  DEBIAN_FRONTEND=noninteractive \
-  UCF_FORCE_CONFOLD=1 \
-    apt-get \
-    -o Dpkg::Options::=--force-confold \
-    -o Dpkg::Options::=--force-confdef \
-    -y -q install apache2 dpkg-dev
+  apt_update
+  install_apache
   echo "Removing temporary repo definition: /etc/apt/sources.list.d/epilocal.list..."
   rm -f /etc/apt/sources.list.d/epilocal.list
-  #rm -f ${epi_repo_server_path}/packages/Packages.gz
-  echo "Updating list of available packages..."
-  apt -y update
+  apt_update
 else
-  # for online mode just install apache
-  echo "Installing apache2 package"
-  # force non-interactive mode, ref: https://bugs.launchpad.net/ubuntu/+source/ansible/+bug/1833013
-  DEBIAN_FRONTEND=noninteractive \
-  UCF_FORCE_CONFOLD=1 \
-    apt-get \
-    -o Dpkg::Options::=--force-confold \
-    -o Dpkg::Options::=--force-confdef \
-    -y -q install apache2 dpkg-dev
-
-  # -m is important because it allow same packages with different versions
-  # 'cd' is needed here becuase 'dpkg-scanpackages' prepends path to "Filename" field in Packages.gz
-  # otherwise it would break package URL for apt
-  cd "${epi_repo_server_path}/packages" && /var/tmp/epi-repository-setup-scripts/dpkg-scanpackages -m . | gzip -9c > Packages.gz && cd "${script_path}"
+  # online mode
+  install_apache
+  generate_repo_metadata "$epi_repo_server_path"
 fi
 
 systemctl start apache2
