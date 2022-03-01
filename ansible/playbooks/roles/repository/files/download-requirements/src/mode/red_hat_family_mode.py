04c37d1458ba17457bc9b705f57e4ed74457743d
to-bar <46519524+to-bar@users.noreply.github.com>
2022-03-01 17:39:19 +0000

    Update URL for epel-release

--- ansible/playbooks/roles/repository/files/download-requirements/src/mode/red_hat_family_mode.py
+++ ansible/playbooks/roles/repository/files/download-requirements/src/mode/red_hat_family_mode.py
@@ -38,7 +38,7 @@ class RedHatFamilyMode(BaseMode):
             if not Path('/etc/yum.repos.d/epel-testing.repo').exists() or not Path('/etc/yum.repos.d/epel.repo').exists():
                 self._tools.yum.remove('epel-release')
 
-        self._tools.yum.install('https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm')
+        self._tools.yum.install('https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm')
         self.__installed_packages.append('epel-release')
 
         self.__remove_yum_cache_for_untracked_repos()
