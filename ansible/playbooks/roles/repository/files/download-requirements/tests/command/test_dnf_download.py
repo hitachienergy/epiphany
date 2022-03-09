da0abcd2b11fa296255a593b366c34d7fc5af6e2
plirglo <plirglo@gmail.com>
2022-03-09 09:04:59 +0100

    Disable subscription-manager plugin for repoquery and download

--- ansible/playbooks/roles/repository/files/download-requirements/tests/command/test_dnf_download.py
+++ ansible/playbooks/roles/repository/files/download-requirements/tests/command/test_dnf_download.py
@@ -15,5 +15,6 @@ def test_interface_download_packages(mocker):
                              'download',
                              '--archlist=some_arch,noarch',
                              '--destdir=/some/path',
+                             '--disableplugin=subscription-manager',
                              '--exclude=*',
                              '--quiet']
