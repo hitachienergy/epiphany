12c60a871712586ddedcaab3877293b8385cbb56
to-bar <46519524+to-bar@users.noreply.github.com>
2022-03-18 10:44:58 +0000

    Fix comment

--- ansible/playbooks/roles/repository/files/download-requirements/conftest.py
+++ ansible/playbooks/roles/repository/files/download-requirements/conftest.py
@@ -1,3 +1,3 @@
 # This file fixes import issues (like ModuleNotFoundError: No module named 'tests.mocks')
-# when discovering or running pytest by adding parent directory to sys.path.
+# when discovering or running tests by adding parent directory to sys.path.
 # See https://stackoverflow.com/a/50610630/10861750
