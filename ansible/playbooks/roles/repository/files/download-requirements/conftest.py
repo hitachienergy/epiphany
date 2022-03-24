ce6a08669ccd42749ebe746adfc82fb643c3642b
to-bar <46519524+to-bar@users.noreply.github.com>
2022-03-24 16:07:20 +0100

    Development improvements (#3032)
    * Use native API and UI for running tests
    
    * Remove obsolete settings
    
    * Fix yum tests
    
    * Fix import issues
    
    * Refactor pytest.ini
    
    * Add download-requirements tests to pytest.ini
    
    * Use default junit_family
    
    * Do not use deprecated distutils package
    
    * Ignore only known warnings
    
    * Fix pylint import issues for download-requirements
    
    * Update py-version
    
    * Fix comment
    
    * Refactor VSCode tasks
    
    * Format tasks.json
    
    * Add "Pylint download-requirements" task

---
+++ ansible/playbooks/roles/repository/files/download-requirements/conftest.py
@@ -0,0 +1,3 @@
+# This file fixes import issues (like ModuleNotFoundError: No module named 'tests.mocks')
+# when discovering or running tests by adding parent directory to sys.path.
+# See https://stackoverflow.com/a/50610630/10861750
