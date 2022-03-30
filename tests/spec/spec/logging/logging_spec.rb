2471b0521704b29039cf134bf1d1a26d4adb4d70
rafzei <webdler@gmail.com>
2022-03-30 13:15:29 +0200

    Tests fix

--- tests/spec/spec/logging/logging_spec.rb
+++ tests/spec/spec/logging/logging_spec.rb
@@ -1,41 +1,44 @@
 require 'spec_helper'
-
 # Configurable passwords for ES users were introduced in v0.10.0.
 # For testing upgrades, we use the default password for now but we're going to switch to TLS auth.
 es_admin_password = readDataYaml('configuration/logging')['specification']['admin_password'] || 'admin'
-
 es_rest_api_port  = 9200
 es_transport_port = 9300
-
-describe 'Check if Elasticsearch service is running' do
-  describe service('elasticsearch') do
+describe 'Check if Opensearch service is running' do
+  describe service('opensearch') do
     it { should be_enabled }
     it { should be_running }
   end
 end
-
-describe 'Check if elasticsearch user exists' do
-  describe group('elasticsearch') do
+describe 'Check if opensearch user exists' do
+  describe group('opensearch') do
     it { should exist }
   end
-  describe user('elasticsearch') do
+  describe user('opensearch') do
     it { should exist }
-    it { should belong_to_group 'elasticsearch' }
+    #it { should belong_to_group 'opensearch' }
   end
 end
-
+describe 'Check if opensearchdboard user exists' do
+  describe group('opensearchdboard') do
+    it { should exist }
+  end
+  describe user('opensearchdboard') do
+    it { should exist }
+    it { should belong_to_group 'opensearchdboard' }
+  end
+end
 describe 'Check Elasticsearch directories and config files' do
   let(:disable_sudo) { false }
-  describe file('/etc/elasticsearch') do
+  describe file('/usr/share/opensearch') do
     it { should exist }
     it { should be_a_directory }
   end
-  describe file('/etc/elasticsearch/elasticsearch.yml') do
+  describe file('/usr/share/opensearch/config/opensearch.yml') do
     it { should exist }
     it { should be_a_file }
   end
 end
-
 describe 'Check if the ports are open' do
   let(:disable_sudo) { false }
   describe port(es_rest_api_port) do
@@ -45,7 +48,6 @@ describe 'Check if the ports are open' do
     it { should be_listening }
   end
 end
-
 listInventoryHosts('logging').each do |val|
   describe 'Check Elasticsearch nodes status codes' do
     let(:disable_sudo) { false }
@@ -56,7 +58,6 @@ listInventoryHosts('logging').each do |val|
     end
   end
 end
-
 listInventoryHosts('logging').each do |val|
   describe 'Check Elasticsearch health' do
     let(:disable_sudo) { false }
