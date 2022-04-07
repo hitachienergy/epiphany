c4b92ba38dcfd396065165191804a12d1d8a9616
cicharka <arkadiusz.cichon@outlook.com>
2022-04-07 21:28:47 +0200

    azure: remove delete_on_termination flag
    * enhance docs
    * imporove delete command

--- cli/src/commands/Delete.py
+++ cli/src/commands/Delete.py
@@ -1,4 +1,5 @@
 from cli.src.helpers.build_io import delete_directory, load_manifest
+from cli.src.helpers.cli_helpers import query_yes_no
 from cli.src.helpers.doc_list_helpers import select_single
 from cli.src.Step import Step
 from cli.src.terraform.TerraformRunner import TerraformRunner
@@ -22,6 +23,11 @@ class Delete(Step):
 
         if cluster_model.provider == 'any':
             raise Exception('Delete works only for cloud providers')
+
+        if cluster_model.provider == 'azure':
+            if not query_yes_no("Terraform will delete existing cluster with all disks belonging to parent Resource Group."
+                                "Do you want to continue?"):
+                sys.exit(0)
 
         with TerraformRunner(cluster_model, docs) as tf_runner:
             tf_runner.destroy()
