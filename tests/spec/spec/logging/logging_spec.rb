edae125c96150483c46c62180e601ef6054f3e8f
rafzei <webdler@gmail.com>
2022-05-17 00:53:40 +0200

    Spec tests update, rebase + changes after review

--- tests/spec/spec/logging/logging_spec.rb
+++ tests/spec/spec/logging/logging_spec.rb
@@ -1,15 +1,28 @@
 require 'spec_helper'
 # Configurable passwords for ES users were introduced in v0.10.0.
 # For testing upgrades, we use the default password for now but we're going to switch to TLS auth.
+es_kibanaserver_user_password  = readDataYaml('configuration/logging')['specification']['kibanaserver_password'] || 'kibanaserver'
+es_kibanaserver_user_is_active = readDataYaml('configuration/logging')['specification']['kibanaserver_user_active']
+es_kibanaserver_user_is_active = true if es_kibanaserver_user_is_active.nil?
 es_admin_password = readDataYaml('configuration/logging')['specification']['admin_password'] || 'admin'
 es_rest_api_port  = 9200
 es_transport_port = 9300
-describe 'Check if OpenSearch service is running' do
+opensearch_dashboards_port = 5601
+
+describe 'Check if opensearch service is running' do
   describe service('opensearch') do
     it { should be_enabled }
     it { should be_running }
   end
 end
+
+describe 'Check if opensearch-dashboard service is running' do
+  describe service('opensearch-dashboards') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+end
+
 describe 'Check if opensearch user exists' do
   describe group('opensearch') do
     it { should exist }
@@ -48,6 +61,7 @@ describe 'Check if the ports are open' do
     it { should be_listening }
   end
 end
+
 listInventoryHosts('logging').each do |val|
   describe 'Check Elasticsearch nodes status codes' do
     let(:disable_sudo) { false }
@@ -57,13 +71,26 @@ listInventoryHosts('logging').each do |val|
       end
     end
   end
-end
-listInventoryHosts('logging').each do |val|
-  describe 'Check Elasticsearch health' do
+  describe 'Check OpenSearch health' do
     let(:disable_sudo) { false }
     describe command("curl -k -u admin:#{es_admin_password} https://#{val}:#{es_rest_api_port}/_cluster/health?pretty=true") do
       its(:stdout_as_json) { should include('status' => /green|yellow/) }
       its(:stdout_as_json) { should include('number_of_nodes' => countInventoryHosts('logging')) }
+      its(:exit_status) { should eq 0 }
+    end
+  end
+  describe 'Check OpenSearch Dashboard HTTP status code' do
+    let(:disable_sudo) { false }
+    describe command("curl -u kibanaserver:#{es_kibanaserver_user_password} -o /dev/null -s -w '%{http_code}' http://#{val}:#{opensearch_dashboards_port}/app/login") do
+      it 'is expected to be equal' do
+        expect(subject.stdout.to_i).to eq 200
+      end
+    end
+  end
+  describe 'Check OpenSearch Dashboards health' do
+    let(:disable_sudo) { false }
+    describe command("curl http://#{val}:#{opensearch_dashboards_port}/api/status") do
+      its(:stdout_as_json) { should include('status' => include('overall' => include('state' => 'green'))) }
       its(:exit_status) { should eq 0 }
     end
   end
