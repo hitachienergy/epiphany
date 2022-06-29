48883f7803f76b8d91a0e1a51a1796a0931bba4c
to-bar <46519524+to-bar@users.noreply.github.com>
2022-06-29 09:00:38 +0000

    Fix for offline mode

--- ansible/playbooks/roles/repository/files/server/RedHat/create-repository.sh
+++ ansible/playbooks/roles/repository/files/server/RedHat/create-repository.sh
@@ -4,7 +4,13 @@ epi_repo_server_path=$1 # /var/www/html/epirepo is the default
 is_offline_mode=$2
 
 if [[ "$is_offline_mode" == "true" ]]; then
-  dnf localinstall --cacheonly --disablerepo='*' -y $(ls "${epi_repo_server_path}"/packages/repo-prereqs/*.rpm)
+  if dnf list installed python3-createrepo_c; then  # install all
+    readarray -t prereq_packages < <(find "${epi_repo_server_path}/packages/repo-prereqs/" -type f -name \*.rpm)
+  else  # skip python3-createrepo_c
+    readarray -t prereq_packages < <(find "${epi_repo_server_path}/packages/repo-prereqs/" -type f -name \*.rpm \
+                                          ! -name python3-createrepo_c\*)
+  fi
+  dnf localinstall --cacheonly --disablerepo='*' -y "${prereq_packages[@]}"
 else
   dnf install -y httpd createrepo
 fi
