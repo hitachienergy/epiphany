hitachienergy/epiphany

commit 4b3f71d16e1a9377d2d996263cb8dec36a61f210

Fix kubernetes configuration doc issue (#3268)
* Fix kubernetes configuration doc issue

* Fix overwriting kubernetes config introduced in #3254

* Fix wrong exception handling in Upgrade mode

* * remove InfrastructureConfigCollector for `any` provider

cli/src/commands/Apply.py
@@ -105,9 +105,12 @@ class Apply(Step):
             tf_runner.apply()
 
     def collect_infrastructure_config(self):
+        if  self.output_mhandler.cluster_model['provider'] == 'any':
+            return
+
         with provider_class_loader(self.output_mhandler.cluster_model.provider,
                                    'InfrastructureConfigCollector')(self.output_mhandler.docs) as config_collector:
-            config_collector.run()
+            self.output_mhandler.update_doc(config_collector.run())  # update kubernetes config doc
 
         # Save manifest again as we have some new information for Ansible apply
         self.output_mhandler.write_manifest()

cli/src/commands/Upgrade.py
@@ -37,7 +37,7 @@ class Upgrade(Step):
         image_registry_doc = {}
         try:
             image_registry_doc = mhandler['configuration/image-registry'][0]
-        except KeyError:
+        except IndexError:
             image_registry_doc = load_schema_obj(schema_types.DEFAULT, 'common', 'configuration/image-registry')
 
         # filter out only image groups with matching features used by the cluster:

cli/src/providers/any/InfrastructureConfigCollector.py
@@ -1,20 +1,0 @@
-from cli.src.helpers.doc_list_helpers import select_single
-from cli.src.Step import Step
-
-
-class InfrastructureConfigCollector(Step):
-
-    def __init__(self, docs):
-        super().__init__(__name__)
-        self.cluster_model = select_single(docs, lambda x: x.kind == 'epiphany-cluster')
-        self.docs = docs
-
-    def __enter__(self):
-        super().__enter__()
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        pass
-
-    def run(self):
-        pass

cli/src/providers/aws/InfrastructureConfigCollector.py
@@ -10,24 +10,19 @@ class InfrastructureConfigCollector(Step):
         self.cluster_model = select_single(docs, lambda x: x.kind == 'epiphany-cluster')
         self.docs = docs
 
-    def __enter__(self):
-        super().__enter__()
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        pass
-
-    def run(self):
+    def run(self) -> dict:
         with APIProxy(self.cluster_model, self.docs) as proxy:
-            self.apply_efs_filesystem_id_for_k8s_pv(proxy)
+            return self.apply_efs_filesystem_id_for_k8s_pv(proxy)
 
-    def apply_efs_filesystem_id_for_k8s_pv(self, proxy):
+    def apply_efs_filesystem_id_for_k8s_pv(self, proxy) -> dict:
         efs_storage_config = select_first(self.docs, lambda x: x.kind == 'infrastructure/efs-storage')
         kubernetes_config = select_first(self.docs, lambda x: x.kind == 'configuration/kubernetes-master')
 
         if self.should_apply_storage_settings(efs_storage_config, kubernetes_config):
             fs_id = proxy.get_efs_id_for_given_token(efs_storage_config.specification.token)
             kubernetes_config.specification.storage.data = {'server': self.get_efs_server_url(fs_id)}
+
+        return kubernetes_config
 
     def should_apply_storage_settings(self, efs_storage_config, kubernetes_config):
         return efs_storage_config is not None and kubernetes_config is not None and \

cli/src/providers/azure/InfrastructureConfigCollector.py
@@ -10,11 +10,11 @@ class InfrastructureConfigCollector(Step):
         self.cluster_model = select_single(docs, lambda x: x.kind == 'epiphany-cluster')
         self.docs = docs
 
-    def run(self):
+    def run(self) -> dict:
         with APIProxy(self.cluster_model, self.docs) as proxy:
-            self.apply_file_share_for_k8s_pv(proxy)
+            return self.apply_file_share_for_k8s_pv(proxy)
 
-    def apply_file_share_for_k8s_pv(self, proxy):
+    def apply_file_share_for_k8s_pv(self, proxy) -> dict:
         storage_share_config = select_first(self.docs, lambda x: x.kind == 'infrastructure/storage-share')
         kubernetes_config = select_first(self.docs, lambda x: x.kind == 'configuration/kubernetes-master')
 
@@ -24,6 +24,8 @@ class InfrastructureConfigCollector(Step):
                 'storage_account_name': storage_share_config.specification.storage_account_name,
                 'storage_account_key': primary_key
             }
+
+        return kubernetes_config
 
     def should_apply_storage_settings(self, storage_share_config, kubernetes_config):
         return storage_share_config is not None and kubernetes_config is not None

cli/src/schema/ManifestHandler.py
@@ -94,10 +94,6 @@ class ManifestHandler:
         for doc in docs:
             self.add_doc(doc)
 
-    def overwrite_docs(self, docs: list[dict]):
-        self.__docs.clear()
-        self.add_docs(docs)
-
     def get_selected_components(self) -> set:
         """
         Return components used in the manifest.

tests/unit/providers/test_provider_class_loader_any.py
@@ -1,7 +1,5 @@
 from cli.src.providers.any.APIProxy import APIProxy
 from cli.src.providers.any.InfrastructureBuilder import InfrastructureBuilder
-from cli.src.providers.any.InfrastructureConfigCollector import \
-    InfrastructureConfigCollector
 from cli.src.providers.provider_class_loader import provider_class_loader
 
 
@@ -13,11 +11,3 @@ def test_provider_class_loader_infrastructurebuilder_any():
 def test_provider_class_loader_apiproxy_any():
     api_proxy = provider_class_loader('any', 'APIProxy')
     assert api_proxy is APIProxy
-
-
-def test_provider_class_loader_infrastructureconfigcollector_any():
-    infrastructure_config_collector = provider_class_loader('any', 'InfrastructureConfigCollector')
-    assert infrastructure_config_collector is InfrastructureConfigCollector
-
-
-

commit e047706769d94e6e720fed979bc2259e1cc20fb6

Fix epicli apply when there is no k8s component used (#3280)
* This bug has been recently introduced in PR #3268

cli/src/commands/Apply.py
@@ -110,7 +110,9 @@ class Apply(Step):
 
         with provider_class_loader(self.output_mhandler.cluster_model.provider,
                                    'InfrastructureConfigCollector')(self.output_mhandler.docs) as config_collector:
-            self.output_mhandler.update_doc(config_collector.run())  # update kubernetes config doc
+            kube_doc = config_collector.run()
+            if kube_doc:
+                self.output_mhandler.update_doc(kube_doc)  # update kubernetes config doc
 
         # Save manifest again as we have some new information for Ansible apply
         self.output_mhandler.write_manifest()