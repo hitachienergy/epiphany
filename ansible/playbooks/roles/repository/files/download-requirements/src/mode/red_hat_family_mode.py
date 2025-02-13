ba0e402869c8fa2e470ebf47a5514c01c230e5e4
przemyslavic <43173646+przemyslavic@users.noreply.github.com>
2025-02-13 12:39:09 +0100

    Switch repmgr packages from 2ndQuadrant to PGDG repository (#3481)
    * Remove 2ndQuadrant debian repositories
    
    * Remove 2ndQuadrant rhel repositories
    
    * Add changelog

--- ansible/playbooks/roles/repository/files/download-requirements/src/mode/red_hat_family_mode.py
+++ ansible/playbooks/roles/repository/files/download-requirements/src/mode/red_hat_family_mode.py
@@ -117,17 +117,6 @@ class RedHatFamilyMode(BaseMode):
             self._tools.dnf_config_manager.add_repo('https://download.docker.com/linux/centos/docker-ce.repo')
             self._tools.dnf.accept_keys()
 
-        # repmgr is supported only with x86_64 architecture
-        if self._cfg.os_arch == OSArch.X86_64:
-            for repo in ('https://dl.2ndquadrant.com/default/release/get/10/rpm',  # for repmgr
-                         'https://dl.2ndquadrant.com/default/release/get/13/rpm'):
-                Command('curl', self._cfg.retries, [repo]) | Command('bash', self._cfg.retries)  # curl {repo} | bash
-
-            # script adds 2 repositories, only 1 is required
-            for repo in ('2ndquadrant-dl-default-release-pg10-debug',
-                         '2ndquadrant-dl-default-release-pg13-debug'):
-                self._tools.dnf_config_manager.disable_repo(repo)
-
         self._tools.dnf.makecache(False, True)
 
     def __remove_dnf_cache_for_custom_repos(self):
@@ -138,7 +127,6 @@ class RedHatFamilyMode(BaseMode):
             return [path for path in paths if path.name.startswith(repo_id)]
 
         repo_ids = [
-            '2ndquadrant',
             'docker-ce',
             'epel',
         ] + [repo['id'] for repo in self._repositories.values()]
